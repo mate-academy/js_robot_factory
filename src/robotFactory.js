1e4005013e6f4c6d1aa2eb0c61f6132f1ff2a6a8
Ihor Hranetskyi <graniecki20@gmail.com>
2020-07-01 22:03:51 +0300

    Solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,88 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipVersion;
 }
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+
+  return this;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+
+  return this;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+
+  return this;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+
+  return this;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  return `Robot: ${this.name}, `
+  + `Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+};
+
+function FlyingRobot(name, weigth, coords, chipVersion) {
+  BaseRobot.call(this, name, weigth, coords, chipVersion);
+  this.coords.z = 0;
 }
 
-function DeliveryDrone() {
-  // implement
+FlyingRobot.prototype = BaseRobot.prototype;
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+
+  return this;
+};
+
+FlyingRobot.prototype.goDown = function(step = -1) {
+  this.coords.z += step;
+
+  return this;
+};
+
+function DeliveryDrone(
+  name,
+  weigth,
+  coords,
+  chipVersion,
+  maxLoadWeight,
+  currentLoad
+) {
+  FlyingRobot.call(this, name, weigth, coords, chipVersion);
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad;
 }
+
+DeliveryDrone.prototype = FlyingRobot.prototype;
+
+DeliveryDrone.prototype.hookLoad = function(loadObj) {
+  if (loadObj.weight < this.maxLoadWeight) {
+    this.currentLoad = loadObj;
+  }
+
+  return this;
+};
+
+DeliveryDrone.prototype.unhookLoad = function() {
+  this.currentLoad = null;
+
+  return this;
+};
 
 module.exports = {
   BaseRobot,
