54390169d2895a25c2a91ee32d09fd37ec8916ca
oggyrock <steelo.sun@gmail.com>
2020-08-17 18:10:10 +0300

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,86 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
-}
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
 
-function FlyingRobot() {
-  // implement
-}
+  this.coords = {
+    x: coords ? coords.x : 0,
+    y: coords ? coords.y : 0,
+  };
 
-function DeliveryDrone() {
-  // implement
-}
+  this.chipVersion = chipVersion;
+};
+
+function FlyingRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+
+  this.coords = {
+    x: coords ? coords.x : 0,
+    y: coords ? coords.y : 0,
+    z: 0,
+  };
+  this.chipVersion = chipVersion;
+};
+
+function DeliveryDrone(name, weight, coords,
+  chipVersion, maxLoadWeight, currentLoad) {
+  this.name = name;
+  this.weight = weight;
+
+  this.coords = {
+    x: coords ? coords.x : 0,
+    y: coords ? coords.y : 0,
+    z: 0,
+  };
+  this.chipVersion = chipVersion;
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad || 0;
+};
+
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  return `Robot: ${this.name}, Chip version: ${this
+    .chipVersion}, Weight: ${this.weight}`;
+};
+
+FlyingRobot.prototype = BaseRobot.prototype;
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
+
+DeliveryDrone.prototype = FlyingRobot.prototype;
+
+DeliveryDrone.prototype.hookLoad = function(cargo) {
+  if (cargo.weight < this.maxLoadWeight) {
+    this.currentLoad = cargo;
+  }
+};
+
+DeliveryDrone.prototype.unhookLoad = function() {
+  this.currentLoad = null;
+};
 
 module.exports = {
   BaseRobot,
