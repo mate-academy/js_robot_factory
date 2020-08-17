4499ab2803c1c2b36d0e614cf8fb8c94995a88dc
RusTron <rusan.tron@gmail.com>
2020-08-18 00:17:55 +0300

    Solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,83 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
-}
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.chipVersion = chipVersion;
+  this.coords = coords;
+  this.weight = weight;
+};
 
-function FlyingRobot() {
-  // implement
-}
+BaseRobot.prototype = {
+  goForward: function(step = 1) {
+    this.coords.y += step;
 
-function DeliveryDrone() {
-  // implement
-}
+    return this.coords.y;
+  },
+  goBack: function(step = 1) {
+    this.coords.y -= step;
+
+    return this.coords.y;
+  },
+  goRight: function(step = 1) {
+    this.coords.x += step;
+
+    return this.coords.x;
+  },
+  goLeft: function(step = 1) {
+    this.coords.x += step;
+
+    return this.coords.x;
+  },
+  getInfo: function() {
+    const robot = `Robot: ${this.name}, `;
+
+    return robot + `Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  },
+};
+
+function FlyingRobot(name, weight, coords, chipVersion) {
+  BaseRobot.call(this, name, weight, coords, chipVersion);
+  this.coords.z = 0;
+};
+
+FlyingRobot.prototype = {
+  goUp: function(step = 1) {
+    this.coords.z += step;
+
+    return this.coords.z;
+  },
+  goDown: function(step = 1) {
+    this.coords.z -= step;
+
+    return this.coords.z;
+  },
+};
+
+Object.setPrototypeOf(FlyingRobot.prototype, BaseRobot.prototype);
+
+function DeliveryDrone(name, weight,
+  coords, chipVersion, maxLoadWeight, currentLoad) {
+  FlyingRobot.call(this, name, weight, coords, chipVersion);
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad;
+};
+
+DeliveryDrone.prototype = {
+  unhookLoad: function() {
+    this.currentLoad = null;
+
+    return this.currnetLoad;
+  },
+  hookLoad: function(obj) {
+    if (obj.weight < this.maxLoadWeight) {
+      this.currentLoad = obj;
+
+      return this.currentLoad;
+    }
+  },
+};
+
+Object.setPrototypeOf(DeliveryDrone.prototype, FlyingRobot.prototype);
 
 module.exports = {
   BaseRobot,
