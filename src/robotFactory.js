5f6d1b3118cb27bd35a5e308b376c0e7c2c73caf
Alina <alina90565@gmail.com>
2020-10-01 17:01:16 +0300

    done robot factory task

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,73 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipVersion;
 }
+
+BaseRobot.prototype.getInfo = function() {
+  const { name, chipVersion, weight } = this;
+
+  return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
+};
+
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+};
 
 function FlyingRobot() {
-  // implement
+  const robot = new BaseRobot(...arguments);
+
+  robot.coords.z = 0;
+
+  return robot;
 }
 
-function DeliveryDrone() {
-  // implement
+FlyingRobot.prototype = BaseRobot.prototype;
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
+
+// eslint-disable-next-line max-len
+function DeliveryDrone(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+  const robot = new FlyingRobot(name, weight, coords, chipVersion);
+
+  robot.maxLoadWeight = maxLoadWeight;
+  robot.currentLoad = currentLoad;
+
+  return robot;
 }
+
+DeliveryDrone.prototype = FlyingRobot.prototype;
+
+DeliveryDrone.prototype.hookLoad = function(cargo) {
+  if (cargo.weight < this.maxLoadWeight) {
+    this.currentLoad = cargo;
+  }
+};
+
+DeliveryDrone.prototype.unhookLoad = function() {
+  this.currentLoad = null;
+};
 
 module.exports = {
   BaseRobot,
