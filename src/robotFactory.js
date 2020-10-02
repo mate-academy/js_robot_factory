b6895a4fac0edd304ec34d4e34be2cf41e54631e
dmitry <akulinind@gmail.com>
2020-10-02 14:03:23 +0300

    implement flying robot

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -27,9 +27,25 @@ BaseRobot.prototype.goLeft = function(step = 1) {
   this.coords.x -= step;
 };
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.getInfo = function() {
+  // eslint-disable-next-line max-len
+  return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+};
+
+function FlyingRobot(name, weight, coords, chipVersion,) {
+  BaseRobot.call(this, ...arguments);
+  this.coords.z = 0;
 }
+
+FlyingRobot.prototype = BaseRobot.prototype;
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
 
 function DeliveryDrone() {
   // implement
