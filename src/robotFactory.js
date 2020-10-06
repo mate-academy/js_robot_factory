4e3bed1bd9e048fd81f0cd9452b3ed6fe5dbc302
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-06 17:03:53 +0300

    solution factory

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,68 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipWersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipWersion;
 }
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  // eslint-disable-next-line max-len
+  return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+};
+
+function FlyingRobot(name, weight, coords, chipWersion) {
+  BaseRobot.apply(this, arguments);
+
+  this.coords.z = 0;
 }
 
-function DeliveryDrone() {
-  // implement
+FlyingRobot.prototype = BaseRobot.prototype;
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
+
+// eslint-disable-next-line max-len
+function DeliveryDrone(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+  FlyingRobot.apply(this, arguments);
+
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad;
 }
+
+DeliveryDrone.prototype = FlyingRobot.prototype;
+
+DeliveryDrone.prototype.hookLoad = function(load) {
+  if (load.weight <= this.maxLoadWeight) {
+    this.currentLoad = load;
+  }
+};
+
+DeliveryDrone.prototype.unhookLoad = function() {
+  this.currentLoad = null;
+};
 
 module.exports = {
   BaseRobot,
