84de8c8994a6b5d424394d77261208528a43a85e
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-10-09 15:55:46 +0300

    create robot factory

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,89 @@
 'use strict';
 
-function BaseRobot() {
+function BaseRobot(name, weight, coords, chipVersion) {
   // implement
+  this.name = name;
+  this.weight = weight;
+  this.chipVersion = chipVersion;
+  this.coords = coords;
 }
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.goForward = function(step = 1) {
+  // write code here
+  this.coords.y += step;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  // write code here
+  this.coords.y -= step;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  // write code here
+  this.coords.x += step;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  // write code here
+  this.coords.x += step;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  return 'Robot: ' + this.name
+  + ', Chip version: ' + this.chipVersion + ', Weight: ' + this.weight;
+};
+
+function FlyingRobot(name, weight, coords, chipVersion) {
+  const testRobot = new BaseRobot(name, weight, coords, chipVersion);
+
+  for (const key in testRobot) {
+    if (testRobot.hasOwnProperty(key)) {
+      this[key] = testRobot[key];
+    }
+  }
+  // Object.setPrototypeOf(FlyingRobot, testRobot);
+  this.coords.z = 0;
 }
 
-function DeliveryDrone() {
-  // implement
+FlyingRobot.prototype = BaseRobot.prototype;
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
+
+function DeliveryDrone(
+  name, weight,
+  coords, chipVersion,
+  maxLoadWeight,
+  currentLoad
+) {
+  const prev = new FlyingRobot(name, weight, coords, chipVersion);
+
+  for (const key in prev) {
+    if (prev.hasOwnProperty(key)) {
+      this[key] = prev[key];
+    }
+  }
+
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad;
 }
+
+DeliveryDrone.prototype = FlyingRobot.prototype;
+
+DeliveryDrone.prototype.hookLoad = function(loadObject) {
+  if (loadObject.weight < this.maxLoadWeight) {
+    this.currentLoad = loadObject;
+  }
+};
+
+DeliveryDrone.prototype.unhookLoad = function(loadedObject) {
+  this.currentLoad = null;
+};
 
 module.exports = {
   BaseRobot,
