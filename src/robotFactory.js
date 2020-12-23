99bc9ba2ddf544aaa4b7dd72bdab8aafe38dab9e
vovador <vovadorko@gmail.com>
2020-12-23 23:45:35 +0200

    Solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,84 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipVersion;
 }
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+
+  return this;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+
+  return this;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+
+  return this;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+
+  return this;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  const { name, chipVersion, weight } = this;
+
+  return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
+};
+
+function FlyingRobot(name, weight, coords, chipVersion) {
+  BaseRobot.call(this, name, weight, coords, chipVersion);
+  this.coords.z = 0;
 }
 
-function DeliveryDrone() {
-  // implement
+FlyingRobot.prototype = BaseRobot.prototype;
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+
+  return this;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+
+  return this;
+};
+
+function DeliveryDrone(name, weight, coords,
+  chipVerion, maxLoadWeight, currentLoad) {
+  FlyingRobot.call(this, name, weight, coords, chipVerion);
+
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad;
 }
+
+DeliveryDrone.prototype = FlyingRobot.prototype;
+
+DeliveryDrone.prototype.hookLoad = function(load) {
+  if (load.weight <= this.maxLoadWeight && this.currentLoad === null) {
+    this.currentLoad = load;
+  }
+
+  return this;
+};
+
+DeliveryDrone.prototype.unhookLoad = function(load) {
+  this.currentLoad = null;
+
+  return this;
+};
 
 module.exports = {
   BaseRobot,
