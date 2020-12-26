55c2be1b63acc816b636dcfd00755c196e00dc5e
gorzer <gorzer.rostyslav@gmail.com>
2020-12-26 13:02:06 +0200

    Solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,71 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipVersion;
 }
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  return `Robot: ${this.name}, `
+  + `Chip version: ${this.chipVersion}, `
+  + `Weight: ${this.weight}`;
+};
+
+function FlyingRobot(name, weight, coords, chipVersion) {
+  BaseRobot.apply(this, [name, weight, coords, chipVersion]);
+  this.coords.z = 0;
 }
 
-function DeliveryDrone() {
-  // implement
+Object.setPrototypeOf(FlyingRobot.prototype, BaseRobot.prototype);
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
+
+function DeliveryDrone(name, weight, coords, chipVersion, maxLW, currentLoad) {
+  FlyingRobot.apply(this, [name, weight, coords, chipVersion]);
+
+  this.maxLoadWeight = maxLW;
+  this.currentLoad = currentLoad;
 }
+
+Object.setPrototypeOf(DeliveryDrone.prototype, FlyingRobot.prototype);
+
+DeliveryDrone.prototype.hookLoad = function(loadObject) {
+  const { weight } = loadObject;
+
+  if (weight <= this.maxLoadWeight && this.currentLoad === null) {
+    this.currentLoad = loadObject;
+  } else {
+    this.currentLoad = this.currentLoad;
+  }
+};
+
+DeliveryDrone.prototype.unhookLoad = function() {
+  this.currentLoad = null;
+};
 
 module.exports = {
   BaseRobot,
