11348d4d01c0b099234bb29d1b77a9a3d8339731
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2020-12-27 12:15:32 +0200

    Created BaseRobot and FlyingRobot

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,12 +1,61 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipVersion;
 }
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+
+  return this;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+
+  return this;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+
+  return this;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+
+  return this;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  const { name, chipVersion, weight } = this;
+
+  return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
+};
+
+function FlyingRobot(name, weight, coords, chipVersion) {
+  BaseRobot.apply(this, arguments);
+
+  this.coords.z = 0;
 }
+
+FlyingRobot.prototype = new BaseRobot();
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+
+  return this;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+
+  return this;
+};
 
 function DeliveryDrone() {
   // implement
