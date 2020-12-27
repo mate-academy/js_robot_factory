b47a2a357a8a7bd338eeba8e71643e43bb38b278
Pavlo Terletskyi <pavloterleckij2@gmail.com>
2020-12-27 19:27:48 +0200

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,82 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipVersion;
 }
 
-function FlyingRobot() {
-  // implement
+BaseRobot.prototype.goForward = function(step = 1) {
+  this.coords.y += step;
+};
+
+BaseRobot.prototype.goBack = function(step = 1) {
+  this.coords.y -= step;
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  this.coords.x -= step;
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  this.coords.x += step;
+};
+
+BaseRobot.prototype.getInfo = function() {
+  const { name, chipVersion, weight } = this;
+
+  return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
+};
+
+// FlyingRobot constructor
+function FlyingRobot(name, weight, coords, chipVersion) {
+  BaseRobot.call(this, name, weight, coords, chipVersion);
+
+  if (!coords || !coords.z) {
+    this.coords.z = 0;
+  }
+
+  this.coords.z = coords.z;
 }
 
-function DeliveryDrone() {
-  // implement
+Object.setPrototypeOf(FlyingRobot.prototype, BaseRobot.prototype);
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
+
+// DeliveryDrone constructor
+function DeliveryDrone(
+  name,
+  weight,
+  coords,
+  chipVersion,
+  maxLoadWeight,
+  currentLoad
+) {
+  FlyingRobot.call(this, name, weight, coords, chipVersion);
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad;
 }
+
+Object.setPrototypeOf(DeliveryDrone.prototype, FlyingRobot.prototype);
+
+DeliveryDrone.prototype.hookLoad = function(newLoad) {
+  if (newLoad.weight <= this.maxLoadWeight && this.currentLoad === null) {
+    this.currentLoad = newLoad;
+  }
+};
+
+DeliveryDrone.prototype.unhookLoad = function() {
+  this.currentLoad = null;
+};
+
+//
 
 module.exports = {
   BaseRobot,
