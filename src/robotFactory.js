aa3447c4736fe6299e189dfae60ae5c913612917
morozoleh <olegmoroz93@gmail.com>
2021-02-20 13:14:01 +0200

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,16 +1,83 @@
 'use strict';
 
-function BaseRobot() {
-  // implement
+function BaseRobot(name, weight, coords, chipVersion) {
+  this.name = name;
+  this.weight = weight;
+  this.coords = coords;
+  this.chipVersion = chipVersion;
 }
 
-function FlyingRobot() {
-  // implement
-}
+BaseRobot.prototype.goForward = function(step = 1) {
+  if (step > 0) {
+    this.coords.y += step;
+  }
+};
 
-function DeliveryDrone() {
-  // implement
-}
+BaseRobot.prototype.goBack = function(step = 1) {
+  if (step > 0) {
+    this.coords.y -= step;
+  }
+};
+
+BaseRobot.prototype.goRight = function(step = 1) {
+  if (step > 0) {
+    this.coords.x += step;
+  }
+};
+
+BaseRobot.prototype.goLeft = function(step = 1) {
+  if (step > 0) {
+    this.coords.x -= step;
+  }
+};
+
+BaseRobot.prototype.getInfo = function() {
+  return (
+    `Robot: ${this.name},\
+ Chip version: ${this.chipVersion},\
+ Weight: ${this.weight}`
+  );
+};
+
+function FlyingRobot(...parameters) {
+  BaseRobot.call(this, parameters);
+
+  this.coords = {
+    z: 0,
+  };
+};
+FlyingRobot.prototype = Object.create(BaseRobot.prototype);
+
+FlyingRobot.prototype.goUp = function(step = 1) {
+  this.coords.z += step;
+};
+
+FlyingRobot.prototype.goDown = function(step = 1) {
+  this.coords.z -= step;
+};
+
+function DeliveryDrone(
+  name, weight, coords, chipVersion, maxLoadWeight, currentLoad
+) {
+  FlyingRobot.call(this, ...arguments);
+  this.maxLoadWeight = maxLoadWeight;
+  this.currentLoad = currentLoad;
+};
+DeliveryDrone.prototype = Object.create(FlyingRobot.prototype);
+
+DeliveryDrone.prototype.hookLoad = function(cargo) {
+  if (cargo.weight > this.maxLoadWeight) {
+    this.currentLoad = null;
+  } else if (this.currentLoad !== null) {
+    this.currentLoad = this.currentLoad;
+  } else {
+    this.currentLoad = cargo;
+  }
+};
+
+DeliveryDrone.prototype.unhookLoad = function() {
+  this.currentLoad = null;
+};
 
 module.exports = {
   BaseRobot,
