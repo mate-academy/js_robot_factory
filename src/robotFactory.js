32ac46915fed9d82fa2b2d2a55aedff0203288ed
Igor Lisogor <illisogori@gmail.com>
2021-05-14 07:22:31 +0300

    robot factory implemented

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,60 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.coords = coords;
+    this.chipVersion = chipVersion;
+    this.coords.x = coords.x || 0;
+    this.coords.y = coords.y || 0;
+  }
+  getInfo() {
+    // eslint-disable-next-line max-len
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+  goForward(steps = 1) {
+    this.coords.y += steps;
+  }
+  goBack(steps = 1) {
+    this.coords.y -= steps;
+  }
+  goRight(steps = 1) {
+    this.coords.x += steps;
+  }
+  goLeft(steps = 1) {
+    this.coords.x -= steps;
+  }
+}
 
-class FlyingRobot {}
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords.z = coords.z || 0;
+  }
+  goUp(steps = 1) {
+    this.coords.z += steps;
+  }
+  goDown(steps = 1) {
+    this.coords.z -= steps;
+  }
+}
 
-class DeliveryDrone {}
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+  hookLoad(cargo) {
+    if (this.currentLoad === null && this.maxLoadWeight >= cargo.weight) {
+      this.currentLoad = cargo;
+    }
+  }
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
