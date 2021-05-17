cc38ce9549fc60202dd054ae5dd04e4ad6f10295
Vitalii Siedykh <siedykh.v@gmail.com>
2021-05-17 14:06:21 +0300

    fixes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -6,8 +6,11 @@ class BaseRobot {
     this.weight = weight;
     this.chipVersion = chipVersion;
     this.coords = {};
-    this.coords.x = position.x ? position.x : 0;
-    this.coords.y = position.y ? position.y : 0;
+
+    this.coords = {
+      x: position.x || 0,
+      y: position.y || 0,
+    };
   }
   getInfo() {
     return `Robot: ${this.name}, `
@@ -35,7 +38,7 @@ class BaseRobot {
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, position, chipVersion) {
     super(name, weight, position, chipVersion);
-    this.coords.z = position.z ? position.z : 0;
+    this.coords.z = position.z || 0;
   }
 
   goUp(step = 1) {
@@ -47,7 +50,14 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, position, chipVersion, maxLoadWeight, currentLoad) {
+  constructor(
+    name,
+    weight,
+    position,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null,
+  ) {
     super(name, weight, position, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
