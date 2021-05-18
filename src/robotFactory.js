90a6353700258835b0b26b132389c2d1f8eee4a4
Yaroslav <gso218695@gmail.com>
2021-05-18 18:18:06 +0300

    one more try

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -7,11 +7,9 @@ class BaseRobot {
     this.coords = coords;
     this.chipVersion = chipVersion;
 
-    if (Object.keys(coords).length === 0) {
-      this.coords = {
-        x: coords.x || 0,
-        y: coords.y || 0,
-      };
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
   }
 
@@ -42,9 +40,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    if (!this.coords.z) {
-      this.coords.z = coords.z || 0;
-    }
+    this.coords.z = coords.z || 0;
   }
 
   goUp(a = 1) {
