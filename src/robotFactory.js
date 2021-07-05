c8a80e6c2331f4696e16f281076c948853037474
John <jozaman96@gmail.com>
2021-07-05 17:11:33 +0300

    clear code

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -54,7 +54,8 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(name, weight, coords,
-    chipVersion, maxLoadWeight, currentLoad) {
+    chipVersion, maxLoadWeight, currentLoad
+  ) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
