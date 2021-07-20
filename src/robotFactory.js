fc473863081a14b62682b071192bd62f96192994
Andriy Shevtsiv <andriy.shevtsiv@gmail.com>
2021-07-20 12:35:05 +0300

    Solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,108 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
 
-class FlyingRobot {}
+    if (!coords.x) {
+      this.coords = {
+        x: 0,
+        y: 0,
+      };
+    } else {
+      this.coords = coords;
+    }
+  }
 
-class DeliveryDrone {}
+  goForward(step) {
+    if (step) {
+      this.coords.y += step;
+    } else {
+      this.coords.y++;
+    }
+  }
+
+  goBack(step) {
+    if (step) {
+      this.coords.y -= step;
+    } else {
+      this.coords.y--;
+    }
+  }
+
+  goRight(step) {
+    if (step) {
+      this.coords.x += step;
+    } else {
+      this.coords.x++;
+    }
+  }
+
+  goLeft(step) {
+    if (step) {
+      this.coords.x -= step;
+    } else {
+      this.coords.x--;
+    }
+  }
+
+  getInfo() {
+    const name = this.name;
+    const chip = this.chipVersion;
+    const weight = this.weight;
+
+    return `Robot: ${name}, Chip version: ${chip}, Weight: ${weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    if (!coords.z) {
+      this.coords = { z: 0 };
+    } else {
+      this.coords = coords;
+    }
+  }
+
+  goUp(step) {
+    if (step) {
+      this.coords.z += step;
+    } else {
+      this.coords.z++;
+    }
+  }
+
+  goDown(step) {
+    if (step) {
+      this.coords.z -= step;
+    } else {
+      this.coords.z--;
+    }
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (cargo.weight <= this.maxLoadWeight
+      && this.currentLoad === null) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
