4ad04f3fb6f1172e20e501e558085716040ecaf4
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2021-08-19 13:26:17 +0300

    Fixed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -2,34 +2,40 @@
 
 class BaseRobot {
   constructor(name, weight, coords, chipVersion) {
+    const { x = 0, y = 0 } = coords;
+
     this.name = name;
     this.weight = weight;
-    this.coords = coords;
+
+    this.coords = {
+      x,
+      y,
+    };
     this.chipVersion = chipVersion;
-
-    if (coords.x === undefined) {
-      this.coords.x = 0;
-    }
-
-    if (coords.y === undefined) {
-      this.coords.y = 0;
-    }
   }
 
   goForward(step = 1) {
-    this.coords.y += step;
+    if (step > 0) {
+      this.coords.y += step;
+    }
   }
 
   goBack(step = 1) {
-    this.coords.y -= step;
+    if (step > 0) {
+      this.coords.y -= step;
+    }
   }
 
   goRight(step = 1) {
-    this.coords.x += step;
+    if (step > 0) {
+      this.coords.x += step;
+    }
   }
 
   goLeft(step = 1) {
-    this.coords.x -= step;
+    if (step > 0) {
+      this.coords.x -= step;
+    }
   }
 
   getInfo() {
@@ -42,17 +48,21 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    if (coords.z === undefined) {
-      this.coords.z = 0;
-    }
+    const { z = 0 } = coords;
+
+    this.coords.z = z;
   }
 
   goUp(step = 1) {
-    this.coords.z += step;
+    if (step > 0) {
+      this.coords.z += step;
+    }
   }
 
   goDown(step = 1) {
-    this.coords.z -= step;
+    if (step > 0) {
+      this.coords.z -= step;
+    }
   }
 }
 
