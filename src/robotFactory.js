c710e3b2800b3c3d40be26348bd5cdc7cce76246
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-08-23 09:17:28 +0300

    third commit

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -64,7 +64,7 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
-    super(name, weight, coords, chipVersion, maxLoadWeight, currentLoad);
+    super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
