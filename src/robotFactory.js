92b9558539cba69991cc2089005a6da1589f873c
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-08-23 13:03:21 +0300

    fix implement z

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -57,11 +57,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(move = 1) {
