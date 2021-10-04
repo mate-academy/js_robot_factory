3b6b76512240030616f3d82b31ed3f2790d5f60c
Ruslan-Mykhalchak <ruslan1993x@gmail.com>
2021-10-04 12:12:42 +0300

    s_2

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -34,12 +34,7 @@ class BaseRobot {
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
-
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
