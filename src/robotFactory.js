4aa88bbce7af8cf3f8352ab4ccd2827eb36d159b
Taras Ulko <internalpeace10@outlook.com>
2021-10-04 18:51:45 +0300

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,95 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
 
-class FlyingRobot {}
+    this.coords = BaseRobot.setCoords(coords);
+  }
 
-class DeliveryDrone {}
+  static setCoords(coords) {
+    const result = {
+      x: 0,
+      y: 0,
+    };
+
+    for (const coord in coords) {
+      if (coords[coord] === undefined) {
+        result[coord] = 0;
+      }
+
+      result[coord] = coords[coord];
+    }
+
+    return result;
+  }
+
+  getInfo() {
+    return (`Robot: ${this.name}, `
+      + `Chip version: ${this.chipVersion}, `
+      + `Weight: ${this.weight}`);
+  }
+
+  goForward(step = 1) {
+    this.coords.y += step;
+  }
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  }
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  }
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(...args) {
+    super(...args);
+
+    this.coords.z = this.coords.z === undefined
+      ? 0
+      : this.coords.z;
+  }
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  }
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (this.currentLoad) {
+      return;
+    }
+
+    if (cargo.weight > this.maxLoadWeight) {
+      return;
+    }
+
+    this.currentLoad = cargo;
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
