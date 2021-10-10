d6ac70b1c5cad351cd3e6b86853a7e69edb9a160
Anton Kulchytskyi <antek1809@gmail.com>
2021-10-10 15:42:30 +0200

    Changes added

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -4,14 +4,17 @@ class BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     this.name = name;
     this.weight = weight;
-    this.coords = coords;
-    this.coords.x = coords.x || 0;
-    this.coords.y = coords.y || 0;
+
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
     this.chipVersion = chipVersion;
   }
   getInfo() {
-    return `Robot: ${
-      this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    return `Robot: ${this.name},`
+    + ` Chip version: ${this.chipVersion},`
+    + ` Weight: ${this.weight}`;
   }
   goForward(step = 1) {
     this.coords.y += step;
@@ -48,7 +51,7 @@ class DeliveryDrone extends FlyingRobot {
     this.currentLoad = currentLoad;
   }
   hookLoad(cargo) {
-    if (cargo.weight <= this.maxLoadWeight && this.currentLoad === null) {
+    if (cargo.weight <= this.maxLoadWeight && !this.currentLoad) {
       this.currentLoad = cargo;
     }
   }
