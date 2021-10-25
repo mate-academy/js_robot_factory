b1116bd51aad952176d5351556f0fe910b09d1db
Mariia Obraztsova <mariiaobraztsova99@gmail.com>
2021-10-25 20:14:30 +0300

    Fixed coordinates initializing

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -5,10 +5,10 @@ class BaseRobot {
     this.name = name;
     this.weight = weight;
 
-    this.coords = (!coords.x) ? {
-      x: 0,
-      y: 0,
-    } : coords;
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
     this.chipVersion = chipVersion;
   }
 
@@ -39,7 +39,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords.z = (coords.z === undefined) ? 0 : coords.z;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
