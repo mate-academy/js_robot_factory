94072861196f46deb74624faeb0017605394d0e1
nadiaH <nadiahrb79@gmailcom>
2021-11-25 23:21:53 +0200

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,75 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
 
-class FlyingRobot {}
+    this.coords = {
+      x: 0, y: 0, ...coords,
+    };
+  }
 
-class DeliveryDrone {}
+  goForward(steps = 1) {
+    this.coords.y += steps;
+  };
+
+  goBack(steps = 1) {
+    this.coords.y -= steps;
+  };
+
+  goRight(steps = 1) {
+    this.coords.x += steps;
+  };
+
+  goLeft(steps = 1) {
+    this.coords.x -= steps;
+  };
+
+  getInfo() {
+    return `Robot: ${this.name},`
+    + ` Chip version: ${this.chipVersion},`
+    + ` Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    this.coords = {
+      x: 0, y: 0, z: 0, ...coords,
+    };
+  }
+
+  goUp(steps = 1) {
+    this.coords.z += steps;
+  };
+
+  goDown(steps = 1) {
+    this.coords.z -= steps;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, chipVersion, coords,
+    maxLoadWeight, currentLoad = null) {
+    super(name, weight, chipVersion, coords);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
