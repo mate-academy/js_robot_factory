7c74bb397565acee0b4bb8d8bc8f7d4fe2a7a2e8
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-23 20:58:08 +0200

    done

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,13 +1,95 @@
 'use strict';
 
-class BaseRobot {}
+const coordsDef = {
+  x: 0, y: 0,
+};
 
-class FlyingRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
+    this.coords = Object.keys(coords).length === 2 ? coords : coordsDef;
+  }
 
-class DeliveryDrone {}
+  goForward(a = 1) {
+    this.coords.y += a;
+  }
+
+  goBack(a = 1) {
+    this.coords.y -= a;
+  }
+
+  goLeft(a = 1) {
+    this.coords.x -= a;
+  }
+
+  goRight(a = 1) {
+    this.coords.x += a;
+  }
+
+  getInfo() {
+    return 'Robot: ' + this.name + ', Chip version: '
+      + this.chipVersion + ', Weight: ' + this.weight;
+  }
+}
+
+const coordsDefFly = {
+  x: 0, y: 0, z: 0,
+};
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords = Object.keys(coords).length === 3 ? coords : coordsDefFly;
+  }
+
+  goUp(a = 1) {
+    this.coords.z += a;
+  }
+
+  goDown(a = 1) {
+    this.coords.z -= a;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion,
+    maxLoadWeight, currentLoad = null) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (cargo.weight <= this.maxLoadWeight && this.currentLoad === null) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
   FlyingRobot,
   DeliveryDrone,
 };
+
+/**
+ *
+DeliveryDrone**
+weight: number
+description: string
+
+- inherits all the methods from `FlyingRobot` and calls its constructor
+- in addition to `FlyingRobot`'s args it takes `maxLoadWeight` and `currentLoad`
+  and saves them.
+- has `hookLoad` method taking a `cargo` object and saving it to a `currentLoad`
+  property if it is empty and the `cargo.weight` is not greater than the
+  `maxLoadWeight` of the drone.
+- if the drone already has `currentLoad` do not change it
+- has `unhookLoad` method, that `currentLoad` property to `null``
+ */
