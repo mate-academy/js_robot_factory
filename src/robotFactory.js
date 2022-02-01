9296ee956951fb721cc5b0efab41a1303eae0129
Mykhailo Ianush <yanushmisha@gmail.com>
2022-02-01 19:00:09 +0200

    checklist

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -6,8 +6,8 @@ class BaseRobot {
     this.weight = weight;
 
     this.coords = {
-      x: coords.x ? coords.x : 0,
-      y: coords.y ? coords.y : 0,
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
 
     this.chipVersion = chipVersion;
@@ -39,7 +39,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords.z = coords.z ? coords.z : 0;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
