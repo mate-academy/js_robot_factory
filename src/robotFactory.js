f134f52c3995ac870be9b45227e4224d1b51de03
Kostya Kovbel <dovkerdota@gmail.com>
2022-02-04 15:04:04 +0200

    ready to reject :O

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,85 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion || 0.1;
 
-class FlyingRobot {}
+    this.coords = coords
+      ? {
+        x: coords.x || 0,
+        y: coords.y || 0,
+      }
+      : {
+        x: 0,
+        y: 0,
+      };
+  };
 
-class DeliveryDrone {}
+  goForward(step = 1) {
+    this.coords.y += step;
+  };
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  };
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  };
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  };
+
+  getInfo() {
+    // eslint-disable-next-line max-len
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, chipVersion);
+
+    this.coords.x = coords.x ? coords.x : 0;
+    this.coords.y = coords.y ? coords.y : 0;
+    this.coords.z = coords.z ? coords.z : 0;
+  }
+  goUp(step = 1) {
+    this.coords.z += step;
+  }
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  };
+
+  hookLoad(cargo) {
+    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    }
+  };
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
+
+module.exports = {
+  BaseRobot,
+  FlyingRobot,
+  DeliveryDrone,
+};
 
 module.exports = {
   BaseRobot,
