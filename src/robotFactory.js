97256edaa66d9209cf52f2255248d4eab75683b9
Marta_Ostrovska <fenderkn@gmail.com>
2022-03-15 18:38:04 +0200

    redundancy removed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -40,11 +40,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -65,10 +61,10 @@ class DeliveryDrone extends FlyingRobot {
   }
 
   hookLoad(cargo) {
-    if (!this.currentLoad) {
-      if (cargo.weight === 0 || cargo.weight <= this.maxLoadWeight) {
-        this.currentLoad = cargo;
-      }
+    if (!this.currentLoad
+      && (cargo.weight === 0
+      || cargo.weight <= this.maxLoadWeight)) {
+      this.currentLoad = cargo;
     }
   }
 
