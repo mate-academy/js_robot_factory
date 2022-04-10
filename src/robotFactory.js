6b2fd84a75eab607427eec12831cd2a0540aa293
Lidiia_Mishchuk <lidiamishchuk@gmail.com>
2022-04-10 17:44:52 +0300

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,124 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
 
-class FlyingRobot {}
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
+    this.chipVersion = chipVersion;
+  }
 
-class DeliveryDrone {}
+  goForward(step = 1) {
+    this.coords.y += step;
+  }
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  }
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  }
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  }
+
+  getInfo() {
+    return `Robot: ${this.name}, `
+    + `Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+      z: coords.z || 0,
+    };
+  }
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  }
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  }
+
+  goForward(step) {
+    super.goForward(step);
+  }
+
+  goBack(step) {
+    super.goBack(step);
+  }
+
+  goLeft(step) {
+    super.goLeft(step);
+  }
+
+  goRight(step) {
+    super.goRight(step);
+  }
+
+  getInfo() {
+    super.getInfo();
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (!this.currentLoad && cargo && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+
+  goUp() {
+    super.goUp();
+  }
+
+  goDown() {
+    super.goDown();
+  }
+
+  goForward(step) {
+    super.goForward(step);
+  }
+
+  goBack(step) {
+    super.goBack(step);
+  }
+
+  goLeft(step) {
+    super.goLeft(step);
+  }
+
+  goRight(step) {
+    super.goRight(step);
+  }
+
+  getInfo() {
+    super.getInfo();
+  }
+}
 
 module.exports = {
   BaseRobot,
