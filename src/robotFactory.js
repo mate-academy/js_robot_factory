b2f23a1e52c4734abd71c6707acc03d2ac5bc9a9
Helen Petrovska <paznachka@gmail.com>
2022-04-26 19:04:25 +0300

    changes were added after review

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -39,9 +39,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    if (!this.coords.z) {
-      this.coords.z = 0;
-    }
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
