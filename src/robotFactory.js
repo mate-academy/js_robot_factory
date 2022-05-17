fe50f313a9e7c4cb3c8fc014ef188fcc3dddac59
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2022-05-17 21:35:52 +0300

    fixed coords assignment

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -6,14 +6,10 @@ class BaseRobot {
     this.weight = weight;
     this.chipVersion = chipVersion;
 
-    if (coords.x === undefined) {
-      this.coords = {
-        x: 0,
-        y: 0,
-      };
-    } else {
-      this.coords = coords;
-    }
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
   }
 
   goForward(step = 1) {
@@ -42,9 +38,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    if (coords.z === undefined) {
-      this.coords.z = 0;
-    }
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
