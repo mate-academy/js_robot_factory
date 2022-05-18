faa095bf3d90cf73d545f32a6f81e51ebc7b1f9b
Anastasiia Sopotnytska <nastyasopotnytska14@gmail.com>
2022-05-18 09:41:13 +0300

    change

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -37,15 +37,8 @@ class BaseRobot {
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
-    this.name = name;
-    this.weight = weight;
 
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
-    this.chipVersion = chipVersion;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
