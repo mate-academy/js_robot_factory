34962267a5136bd5472b9876fd1cf3b23933ff8e
Karina Prozner <prozner.karina@gmail.com>
2022-05-24 21:50:03 +0200

    changed coords syntax

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,15 +1,14 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, position, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
     this.name = name;
     this.weight = weight;
 
-    this.coords = position.x === undefined
-      ? {
-        x: 0, y: 0,
-      }
-      : position;
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
     this.chipVersion = chipVersion;
   }
 
@@ -33,14 +32,10 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, position, chipVersion) {
-    super(name, weight, position, chipVersion);
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
 
-    this.coords = position.z === undefined
-      ? {
-        z: 0,
-      }
-      : position;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -52,8 +47,8 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, position, chipVersion, maxLoadWeight, currentLoad) {
-    super(name, weight, position, chipVersion);
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
 
