b8f1e3ff1386c4824fb45f49b6647e4c27071bb1
Dmytro Kaunin <kaunin1198@te.net.ua>
2022-06-21 14:10:42 +0300

    Second commit / delivery drone- current load adjustment

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -58,7 +58,7 @@ class DeliveryDrone extends FlyingRobot {
   constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
-    this.currentLoad = currentLoad;
+    this.currentLoad = currentLoad || null;
   }
 
   hookLoad(cargo) {
