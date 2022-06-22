1994bc218c420fb69f68ecbdd67554d21dc2b114
Ostap Salo <jlumon211@gmail.com>
2022-06-22 12:23:40 +0300

    update

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -60,7 +60,7 @@ class DeliveryDrone extends FlyingRobot {
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
-    this.currentLoad = currentLoad;
+    this.currentLoad = currentLoad || null;
   }
 
   hookLoad(cargo) {
