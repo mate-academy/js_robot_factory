a9ec45b9e5a27ee3a28f6b60b71fb312c5e10627
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-06-23 17:50:14 +0300

    Some fixes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -39,11 +39,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -56,9 +52,7 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords = {
-    x: 0, y: 0, z: 0,
-  }, chipVersion, maxLoadWeight, currentLoad) {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
