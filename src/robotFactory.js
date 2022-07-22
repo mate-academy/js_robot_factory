4df32abb43c6d348aa5674bfa03e6d918e05acd1
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-07-22 17:40:51 +0300

    fixed constructor & some blocks of code

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -30,18 +30,9 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, { x = 0, y = 0, z = 0 }, chipVersion) {
-    super(name, weight, {
-      x, y, z,
-    },
-    chipVersion);
-    this.chipVersion = chipVersion;
-
-    this.coords = {
-      x,
-      y,
-      z,
-    };
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords.z = coords.z || 0;
   }
   goUp(step = 1) {
     this.coords.z += step;
@@ -53,12 +44,9 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(name, weight,
-    { x = 0, y = 0, z = 0 }, chipVersion, maxLoadWeight, currentLoad = null) {
-    super(name, weight, {
-      x, y, z,
-    }, chipVersion);
+    coords, chipVersion, maxLoadWeight, currentLoad = null) {
+    super(name, weight, coords, chipVersion);
     this.currentLoad = currentLoad;
-    this.chipVersion = chipVersion;
     this.maxLoadWeight = maxLoadWeight;
   }
   hookLoad(cargo) {
