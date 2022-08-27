70c4c67dcd9cf5109d92b17050b38092df88779c
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-08-27 22:13:07 +0300

    readment fixes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -47,12 +47,7 @@ class FlyingRobot extends BaseRobot {
     coords,
     chipVersion
   ) {
-    super(
-      name,
-      weight,
-      coords,
-      chipVersion
-    );
+    super(name, weight, coords, chipVersion);
 
     this.coords.z = coords.z || 0;
   }
@@ -75,12 +70,7 @@ class DeliveryDrone extends FlyingRobot {
     maxLoadWeight,
     currentLoad = null
   ) {
-    super(
-      name,
-      weight,
-      coords,
-      chipVersion
-    );
+    super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
