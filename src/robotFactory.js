c0598ceaed1f32f113aec202177515c11e9cfcd0
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-08-28 08:09:52 +0300

    sulution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -37,7 +37,9 @@ class BaseRobot {
   }
 
   getInfo() {
-    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    return `Robot: ${this.name}, `
+    + `Chip version: ${this.chipVersion}, `
+    + `Weight: ${this.weight}`;
   }
 }
 
