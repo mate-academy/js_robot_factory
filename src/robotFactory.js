bbe9090897bce3df35aec446bdab1f4cbdfc0279
Adrian Faryniuk <forever0754@gmail.com>
2022-08-30 23:15:57 +0300

    changed code style

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,10 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, coords = {}, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
     this.coords = {
-      x: 0 + coords.x || 0,
-      y: 0 + coords.y || 0,
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
 
     this.name = name;
@@ -35,7 +35,7 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords = {}, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
     this.coords.z = coords.z || 0;
@@ -54,7 +54,7 @@ class DeliveryDrone extends FlyingRobot {
   constructor(
     name,
     weight,
-    coords = {},
+    coords,
     chipVersion,
     maxLoadWeight,
     currentLoad = null
