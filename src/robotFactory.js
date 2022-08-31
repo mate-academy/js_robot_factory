0f262ee2adf90a072c04446cdada1910b48c978e
Andrii Dunets <davbiznesman@gmail.com>
2022-08-31 17:02:11 +0300

    Base Robot class done

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,6 +1,33 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.coords = coords;
+    this.chipVersion = chipVersion;
+  }
+
+  goForward(step = 0) {
+    this.coords.y += step;
+  };
+
+  goBack(step = 0) {
+    this.coords.y -= step;
+  };
+
+  goLeft(step = 0) {
+    this.coords.x -= step;
+  };
+
+  goRight(step = 0) {
+    this.coords.x += step;
+  };
+
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
 
 class FlyingRobot {}
 
