502bda278819d43a4f195d80147bbbe674307a6b
Viktor Kotik <vikuso1996@gmail.com>
2022-09-14 11:59:58 +0300

    Solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -30,8 +30,9 @@ class BaseRobot {
   };
 
   getInfo() {
-    // eslint-disable-next-line
-    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    return `Robot: ${this.name}, `
+    + `Chip version: ${this.chipVersion}, `
+    + `Weight: ${this.weight}`;
   };
 }
 
