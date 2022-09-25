3579e5b54569089aed263d5110ff8c9c7e89145a
Killdex <kdcompany2018@gmail.com>
2022-09-25 13:49:28 +0300

    robot class created

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,125 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
 
-class FlyingRobot {}
+    if (Object.keys(coords).length === 0) {
+      this.coords = {
+        x: 0,
+        y: 0,
+      };
+    } else {
+      this.coords = {
+        x: coords.x || 0,
+        y: coords.y || 0,
+      };
+    }
+  }
 
-class DeliveryDrone {}
+  goForward(step = 1) {
+    this.coords.y += step;
+  }
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  }
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  }
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  }
+
+  getInfo() {
+    return 'Robot: ' + this.name + ', '
+      + 'Chip version: ' + this.chipVersion + ', '
+      + 'Weight: ' + this.weight;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    if (Object.keys(coords).length === 0) {
+      this.coords.z = 0;
+    } else {
+      this.coords.z = coords.z || 0;
+    }
+  }
+
+  goForward() {
+    super.goForward();
+  }
+
+  goBack() {
+    super.goBack();
+  }
+
+  goLeft() {
+    super.goLeft();
+  }
+
+  goRight() {
+    super.goRight();
+  }
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  }
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad || null;
+  }
+
+  goForward() {
+    super.goForward();
+  }
+
+  goBack() {
+    super.goBack();
+  }
+
+  goLeft() {
+    super.goLeft();
+  }
+
+  goRight() {
+    super.goRight();
+  }
+
+  goUp() {
+    super.goUp();
+  }
+
+  goDown() {
+    super.goDown();
+  }
+
+  hookLoad(cargo) {
+    if (this.currentLoad === null && this.maxLoadWeight >= cargo.weight) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
