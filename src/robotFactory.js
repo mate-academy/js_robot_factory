77f8090ddf5bca38d340be18352059b7b32ebf8a
fatihsamur <a.fatihsamur@gmail.com>
2022-09-27 03:42:00 +0300

    initial commit

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,6 +1,13 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.coords = coords;
+    this.chipVersion = chipVersion;
+  }
+}
 
 class FlyingRobot {}
 
