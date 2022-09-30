daec7816ca0e16ba72e2a1a6122a61d87eda95ff
Maksym845 <mzuravel845@gmail.com>
2022-09-30 15:30:49 +0300

    small fix was added

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -29,8 +29,9 @@ class BaseRobot {
   };
 
   getInfo() {
-    return `Robot: ${this.name}, `
-+ `Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    const { name, chipVersion, weight } = this;
+
+    return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
   }
 }
 
@@ -51,8 +52,9 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight,
-    coords, chipVersion, maxLoadWeight, currentLoad = null) {
+  constructor(
+    name, weight, coords, chipVersion, maxLoadWeight, currentLoad = null
+  ) { // did you mean this?
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
