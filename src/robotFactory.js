3d7626762bb8a2242d25f1fe365f9e674af8e17f
Anton Vaida <tohan1.ua@gmail.com>
2022-09-30 14:27:19 +0300

    create destructure

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -29,8 +29,9 @@ class BaseRobot {
   }
 
   getInfo() {
-    return `Robot: ${this.name},`
-    + ` Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    const { name, chipVersion, weight } = this;
+
+    return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
   }
 }
 
