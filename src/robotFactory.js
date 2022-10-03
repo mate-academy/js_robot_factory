535c9de2221997cfef6322c363e057cf3b91fca3
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-10-03 15:43:15 +0300

    The shorthand was used

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -67,7 +67,7 @@ class DeliveryDrone extends FlyingRobot {
   }
 
   hookLoad(cargo) {
-    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
   }
