f786701b5681baf24d86ae445fc4aa0d23f9668b
Andrew Satyr <and.satur@gmail.com>
2022-10-03 14:33:20 +0300

    Fixed unexepected coords

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -39,8 +39,7 @@ class FlyingRobot extends BaseRobot {
     super(name, weight, coords, chipVersion);
 
     this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
+      ...this.coords,
       z: coords.z || 0,
     };
   }
