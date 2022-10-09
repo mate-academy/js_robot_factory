349f61ae34cdb7e46acaca06cc6f978285a6cb65
Oleksandr <Khabailiuk@gmail.com>
2022-10-09 23:46:17 +0200

    second commit

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -39,13 +39,9 @@ class BaseRobot {
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
-
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
+
   goForward() {
     super.goForward();
   }
@@ -80,34 +76,7 @@ class DeliveryDrone extends FlyingRobot {
     }
   }
 
-  goForward() {
-    super.goForward();
-  }
-
-  goLeft() {
-    super.goLeft();
-  }
-
-  goBack() {
-    super.goBack();
-  }
-
-  goRight() {
-    super.goRight();
-  }
-
-  goUp(step) {
-    super.goUp();
-  }
-
-  goDown(step) {
-    super.goDown();
-  };
-
   hookLoad(cargo) {
-    if (this.currentLoad) {
-      return this.currentLoad;
-    } else
     if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
