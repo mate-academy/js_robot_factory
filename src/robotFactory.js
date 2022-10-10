ff4d671788731b869726661547dab58c99cf62e6
Kolya Movchan <kmovchan261@gmail.com>
2022-10-10 23:31:31 +0300

    updated 2.0

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -40,7 +40,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    const { x: x = 0, y: y = 0, z: z = 0 } = coords;
+    const { x, y, z: z = 0 } = coords;
 
     this.coords = {
       x,
@@ -67,7 +67,7 @@ class DeliveryDrone extends FlyingRobot {
   }
 
   hookLoad(cargo) {
-    if (cargo.weight <= this.maxLoadWeight && this.currentLoad === null) {
+    if (cargo.weight <= this.maxLoadWeight && !this.currentLoad) {
       this.currentLoad = cargo;
     }
   };
