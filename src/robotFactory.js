c7c27dd1b65be0dfad13d0dfc7ddefd4350a884e
John Rust <johnrust12@gmail.com>
2022-10-11 11:16:20 +0300

    Solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,134 @@
 'use strict';
 
-class BaseRobot {}
+// BaseRobot
 
-class FlyingRobot {}
+// constructor takes name, weight, coords, chipVersion and saves them
+// coords should be set to 0 if not passed
+// goForward, goBack, goRight and goLeft methods take a step
+// argument (1 by default) and move the robot by step
+// in the appropriate direction
+// getInfo method returns a string in the next format
+// Robot: %name%, Chip version: %chipVersion%, Weight: %weight%
 
-class DeliveryDrone {}
+// FlyingRobot
+
+// inherits all the methods from BaseRobot
+// takes the same args as BaseRobot and passes them to the parent's constructor
+// can work with z coords
+// has methods goUp and goDown changing z coordinate by a given step
+
+// DeliveryDrone
+
+// inherits all the methods from FlyingRobot and calls its constructor
+// in addition to FlyingRobot's args it takes maxLoadWeight and
+// currentLoad and saves them.
+// currentLoad should be set to null if not passed
+// has hookLoad method taking a cargo object and
+// saving it to a currentLoad property if it is empty and
+// the cargo.weight is not greater than the maxLoadWeight of the drone.
+// if the drone already has currentLoad do not change it
+// has unhookLoad method, that currentLoad property to null
+// DeliveryDrone {
+//   name: string
+//   weight: number
+//   chipVersion: number
+//   maxLoadWeight: number
+//   currentLoad: null || {
+//     weight: number
+//     description: string
+//   }
+//   coords: {
+//     x: number
+//     y: number
+//     z: number
+//   }
+
+class BaseRobot {
+  constructor(name, weight, { x = 0, y = 0 }, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+
+    this.coords = {
+      x: x,
+      y: y,
+    };
+
+    this.chipVersion = chipVersion;
+  }
+
+  goForward(step = 1) {
+    this.coords.y += step;
+  };
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  };
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  };
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  };
+
+  getInfo() {
+    return 'Robot: '
+      + this.name + ', '
+      + 'Chip version: '
+      + this.chipVersion + ', '
+      + 'Weight: '
+      + this.weight;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, { x = 0, y = 0, z = 0 }, chipVersion) {
+    super(
+      name,
+      weight,
+      {
+        x, y,
+      },
+      chipVersion);
+
+    this.coords.z = z;
+  }
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  };
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  };
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(
+    name,
+    weight,
+    { x = 0, y = 0, z = 0 },
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
+    super(name, weight, x, y, z, chipVersion);
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
