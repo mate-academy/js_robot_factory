0ce8f0f257dbab9bdf6b0423cd10cac5e16821b5
Roman Starovoit <rstarovoit@gmail.com>
2022-10-12 17:58:00 +0300

    getInfo destructuring

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -29,9 +29,12 @@ class BaseRobot {
   };
 
   getInfo() {
-    return 'Robot: ' + this.name + ', '
-      + 'Chip version: ' + this.chipVersion + ', '
-      + 'Weight: ' + this.weight;
+    const { name, chipVersion: version, weight } = this;
+
+    return `Robot: ${name}, Chip version: ${version}, Weight: ${weight}`;
+    // return 'Robot: ' + this.name + ', '
+    //   + 'Chip version: ' + this.chipVersion + ', '
+    //   + 'Weight: ' + this.weight;
   }
 }
 
