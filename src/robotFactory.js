1505c73e4ab3028a973f98f16b48d1043682abdd
Yuri Gatskovskyi <yurimoreau@gmail.com>
2022-10-14 15:34:20 +0300

    fixed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,14 +1,14 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, { x = 0, y = 0 }, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
     this.name = name;
     this.weight = weight;
     this.chipVersion = chipVersion;
 
     this.coords = {
-      x: x,
-      y: y,
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
   }
   goForward(step = 1) {
@@ -32,11 +32,9 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, { x = 0, y = 0, z = 0 }, chipVersion) {
-    super(name, weight, {
-      x, y,
-    }, chipVersion);
-    this.coords.z = z;
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords.z = coords.z || 0;
   }
   goUp(step = 1) {
     this.coords.z += step;
@@ -47,11 +45,9 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, { x = 0, y = 0, z = 0 }, chipVersion,
+  constructor(name, weight, coords, chipVersion,
     maxLoadWeight, currentLoad = null) {
-    super(name, weight, {
-      x, y, z,
-    }, chipVersion);
+    super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
