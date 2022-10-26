d1ec6d02270e6b2da0cb863f056e684d64d54e81
Dmytro Legerda <terrain1984@gmail.com>
2022-10-26 20:11:47 +0300

    Fixes after code review(2)

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name = '', weight = 0, coords, chipVersion = 0) {
+  constructor(name, weight, coords, chipVersion) {
     this.name = name;
     this.weight = weight;
 
