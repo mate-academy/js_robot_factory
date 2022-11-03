137af0e86febd01ca522df50021337ed34f03146
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-11-03 22:11:55 +0200

    fix

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -2,18 +2,13 @@
 
 class BaseRobot {
   constructor(name, weight, coords, chipVersion) {
-    const {
-      x = 0,
-      y = 0,
-    } = coords;
-
     this.name = name;
     this.weight = weight;
     this.chipVersion = chipVersion;
 
     this.coords = {
-      x,
-      y,
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
   }
 
@@ -43,17 +38,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    const {
-      x = 0,
-      y = 0,
-      z = 0,
-    } = coords;
-
-    this.coords = {
-      x,
-      y,
-      z,
-    };
+    this.coords.z = coords.z || 0;
   }
   goUp(step = 1) {
     this.coords.z += step;
