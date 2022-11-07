8f558b5b9bc2bf85a11f77f1c68202f2639e0ede
Olena Shevchenko <shevochkas@gmail.com>
2022-11-07 22:25:42 +0200

    code robot factory classes was improved

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -55,12 +55,7 @@ class DeliveryDrone extends FlyingRobot {
   constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
-
-    if (currentLoad) {
-      this.currentLoad = currentLoad;
-    } else {
-      this.currentLoad = null;
-    }
+    this.currentLoad = currentLoad || null;
   }
 
   hookLoad(cargo) {
