d296ea58bd48fb2c746d88b7cd78f9fae4b4c06b
Taras Pustovyi <taraselnik@gmail.com>
2022-11-08 23:17:42 +0200

    fix woring with arguments

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,13 +1,14 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, { x = 0, y = 0 }, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
     this.name = name;
     this.weight = weight;
     this.chipVersion = chipVersion;
 
     this.coords = {
-      x, y,
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
   }
 
@@ -34,9 +35,10 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(...args) {
-    super(...arguments);
-    this.coords.z = arguments[2].z || 0;
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -50,7 +52,12 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(name,
-    weight, coords, chipVersion, maxLoadWeight, currentLoad = null) {
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
     super(...arguments);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
