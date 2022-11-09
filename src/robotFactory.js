8d23ce7656c912f2c3a53fb996a3393357cdad03
Diana <dianaovb444@gmail.com>
2022-11-09 10:56:42 +0200

    fixed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -56,8 +56,14 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion, maxLoadWeight,
-    currentLoad = null) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
