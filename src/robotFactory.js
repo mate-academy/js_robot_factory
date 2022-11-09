1741a37c0cbb7891c2c1906cdb6f21c521305106
Danil Serzhantov <sergo.svd@gmail.com>
2022-11-09 18:44:08 +0200

    v3

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -30,8 +30,10 @@ class BaseRobot {
   }
 
   getInfo() {
-    return `Robot: ${this.name}, `
-     + `Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    const { name, chipVersion, weight } = this;
+
+    return `Robot: ${name}, `
+     + `Chip version: ${chipVersion}, Weight: ${weight}`;
   }
 }
 
@@ -52,11 +54,12 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+  constructor(name, weight, coords,
+    chipVersion, maxLoadWeight, currentLoad = null) {
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
-    this.currentLoad = currentLoad || null;
+    this.currentLoad = currentLoad;
   }
 
   hookLoad(cargo) {
