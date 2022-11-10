22052fd3838ee13f3ae0cf1346f6037dc76b2556
oksenyuk-valentyna <vioksenyuk@gmail.com>
2022-11-10 10:44:15 +0200

    some fixes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -29,8 +29,10 @@ class BaseRobot {
   };
 
   getInfo() {
-    return `Robot: ${this.name},`
-    + ` Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    const { name, chipVersion, weight } = this;
+
+    return `Robot: ${name},`
+    + ` Chip version: ${chipVersion}, Weight: ${weight}`;
   };
 }
 
@@ -50,10 +52,17 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
-    this.currentLoad = currentLoad || null;
+    this.currentLoad = currentLoad;
   };
 
   hookLoad(cargo) {
