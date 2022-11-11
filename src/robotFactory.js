f8ae5bc4d2a1be6a397538952b2247e1e45b4818
Yurii Ostapchuk <ostapchuk.y.o@gmail.com>
2022-11-11 13:32:01 +0200

    develop - 2

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -93,7 +93,12 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(
-    name, weight, coords, chipVersion, maxLoadWeight, currentLoad = null
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
   ) {
     super(name, weight, coords, chipVersion);
 
