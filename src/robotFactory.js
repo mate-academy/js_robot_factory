35aa68997bd752757910c4856a8e1cdf0fa5f0aa
Taras Vorobets <97515300+TarasV123@users.noreply.github.com>
2022-11-17 18:17:26 +0200

    Update src/robotFactory.js
    Co-authored-by: Tatiana Ivanchenko <59209337+tetiana-ivanchenko@users.noreply.github.com>

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -40,11 +40,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = {
-      ...coords,
-
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
