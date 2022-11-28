4690ee956b43de9735151f8d5d7aa993f7cc31bf
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2022-11-28 15:42:17 +0100

    commit

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,56 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
 
-class FlyingRobot {}
+    this.coords = {
+      x: 0,
+      y: 0,
+    };
+  }
+  goForward(value = 1) {
+    if (value > 0) {
+      this.coords.y += value;
+    }
+  }
+  goBack(value = 1) {
+    if (value > 0) {
+      this.coords.y -= value;
+    }
+  }
+  goRight(value = 1) {
+    if (value > 0) {
+      this.coords.x += value;
+    }
+  }
+  goLeft(value = 1) {
+    if (value > 0) {
+      this.coords.x -= value;
+    }
+  }
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion},
+     Weight: ${this.weight}`;
+  }
+}
 
-class DeliveryDrone {}
+class FlyingRobot extends BaseRobot {
+  goUp(value = 1) {
+    if (value > 1) {
+      this.coords.z += value;
+    }
+  };
+  goDown(value = 1) {
+    if (value > 1) {
+      this.coords.z -= value;
+    };
+  }
+}
+class DeliveryDrone extends FlyingRobot {
+}
 
 module.exports = {
   BaseRobot,
