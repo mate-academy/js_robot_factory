6e2276eb5477caa7468efbbf8b5968b06783d61e
kreisberg <anastasia.kreisberg@yahoo.com>
2022-12-13 11:32:27 +0200

    fixes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,7 +1,9 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
+  constructor(
+    name, weight, coords, chipVersion
+  ) {
     this.name = name;
     this.weight = weight;
 
@@ -36,7 +38,9 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
+  constructor(
+    name, weight, coords, chipVersion
+  ) {
     super(...arguments);
     this.coords.z = coords.z || 0;
   }
@@ -51,8 +55,10 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion,
-    maxLoadWeight, currentLoad = null) {
+  constructor(
+    name, weight, coords, chipVersion,
+    maxLoadWeight, currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
