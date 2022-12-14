9c705a172f22dc5b3ccbf3a13269f2c07e0e4501
Diana Demkiv <dianademkiv789@gmail.com>
2022-12-14 10:40:22 +0200

    fixed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -5,9 +5,10 @@ class BaseRobot {
     this.name = name;
     this.weight = weight;
 
-    this.coords = coords;
-    this.coords.x = this.coords.x || 0;
-    this.coords.y = this.coords.y || 0;
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
 
     this.chipVersion = chipVersion;
   }
@@ -37,7 +38,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords.z = this.coords.z || 0;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
