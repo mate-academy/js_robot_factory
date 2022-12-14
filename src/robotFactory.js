7bce78e26b1852aa995b7285a2512d9710fed0b7
Gürsel Çakır <hardmatters@outlook.com>
2022-12-14 20:45:21 +0300

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -55,12 +55,7 @@ class DeliveryDrone extends FlyingRobot {
   constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
-
-    if (!currentLoad) {
-      this.currentLoad = null;
-    } else {
-      this.currentLoad = currentLoad;
-    }
+    this.currentLoad = currentLoad || null;
   }
 
   hookLoad(cargo) {
