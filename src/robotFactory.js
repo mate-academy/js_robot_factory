b5003f300943f36987c2e699bcc338b9f0675818
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-15 19:58:56 +0200

    added DeliveryDrone class, and changed other 2 classes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -6,27 +6,28 @@ class BaseRobot {
     this.weight = weight;
     this.chipVersion = chipVersion;
 
-    this.coords = coords || {
-      x: 0, y: 0,
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
   }
 
   goForward(step = 1) {
-    this.y += step;
+    this.coords.y += step;
   }
   goBack(step = 1) {
-    this.y -= step;
+    this.coords.y -= step;
   }
   goRight(step = 1) {
-    this.x += step;
+    this.coords.x += step;
   }
   goLeft(step = 1) {
-    this.x -= step;
+    this.coords.x -= step;
   }
   getInfo() {
     return `Robot: ${this.name},`
-      + `Chip version: ${this.chipVersion},`
-      + `Weight: ${this.weight}`;
+      + ` Chip version: ${this.chipVersion},`
+      + ` Weight: ${this.weight}`;
   }
 }
 
@@ -34,21 +35,37 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = coords || {
-      x: 0, y: 0, z: 0,
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+      z: coords.z || 0,
     };
   }
 
   goUp(step = 1) {
-    this.z += step;
+    this.coords.z += step;
   }
   goDown(step = 1) {
-    this.z -= step;
+    this.coords.z -= step;
   }
 }
 
 class DeliveryDrone extends FlyingRobot {
-  
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad || null;
+  }
+
+  hookLoad(cargo) {
+    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
 }
 
 module.exports = {
