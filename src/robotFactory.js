8498de9788e12bc58774899a2b53636e24a1d994
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-15 17:00:31 +0200

    added BaseRobot class

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,6 +1,34 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
+
+    this.coords = coords || {
+      x: 0, y: 0,
+    };
+  }
+
+  goForward(step = 1) {
+    this.y += step;
+  }
+  goBack(step = 1) {
+    this.y -= step;
+  }
+  goRight(step = 1) {
+    this.x += step;
+  }
+  goLeft(step = 1) {
+    this.x -= step;
+  }
+  getInfo() {
+    return `Robot: ${this.name},`
+      + `Chip version: ${this.chipVersion},`
+      + `Weight: ${this.weight}`;
+  }
+}
 
 class FlyingRobot {}
 
