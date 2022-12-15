34c61e8a105d64e9c17959956bb49dcd54a74679
Vladimir Arkanov <vladimirarkanov@icloud.com>
2022-12-15 16:22:37 +0200

    solution 3x

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -39,11 +39,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = {
-      x: coords.x,
-      y: coords.y,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
