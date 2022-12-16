8a12dc51c4634861765fdd6d68cbf4ad733075b1
volkan yildiz <yildizvolkan91@gmail.com>
2022-12-16 20:17:40 +0300

    sol4

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -24,8 +24,8 @@ class BaseRobot {
     this.coords.x -= step;
   }
   getInfo() {
-    return `Robot: ${this.name},
-    Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+    return `Robot: ${this.name}, `
+    + `Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
   }
 }
 
