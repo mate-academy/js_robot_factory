a769d4a4c89e62d7a3515ba4d79b07b0756ebcfc
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-20 11:29:38 +0200

    Solution after changes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -37,8 +37,12 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, { z = 0, ...coords }, chipVersion) {
-    super(name, weight, coords, chipVersion);
+  constructor(name, weight, { x, y, z = 0 }, chipVersion) {
+    super(name, weight, {
+      x: x,
+      y: y,
+      z: z,
+    }, chipVersion);
 
     this.coords.z = z;
   }
@@ -55,11 +59,15 @@ class FlyingRobot extends BaseRobot {
 class DeliveryDrone extends FlyingRobot {
   constructor(name,
     weight,
-    { ...coords },
+    { x, y, z },
     chipVersion,
     maxLoadWeight,
     currentLoad = null) {
-    super(name, weight, { ...coords }, chipVersion);
+    super(name, weight, {
+      x: x,
+      y: y,
+      z: z,
+    }, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
   }
