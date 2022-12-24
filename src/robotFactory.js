4af311e5e10e9fe8cb2c1102ab3524b128db2a1f
EhorSaprykin <astralik1993@gmail.com>
2022-12-24 17:02:39 +0200

    BaseRobot class done

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,6 +1,39 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
+
+    this.chipVersion = chipVersion;
+  }
+
+  goForward(step = 1) {
+    this.coords.y += step;
+  }
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  }
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  }
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  }
+
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: ${
+      this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
 
 class FlyingRobot {}
 
