30e1bbd6daf168e19642f775f74c32f155e0ad8e
Your Name <you@example.com>
2023-01-17 12:20:26 +0200

    fixed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -57,7 +57,8 @@ class DeliveryDrone extends FlyingRobot {
     coords,
     chipVersion,
     maxLoadWeight,
-    currentLoad = null) {
+    currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
