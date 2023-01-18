91673f97ad5ff4952f54f7a93573cf11ec90e839
Oleksandr Prokopenko <37651667+AlexPro100pro@users.noreply.github.com>
2023-01-18 15:04:13 +0200

    Update src/robotFactory.js
    Co-authored-by: Artur Ermolenko <94292654+ArturErmolenko@users.noreply.github.com>

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -49,8 +49,14 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords,
-    chipVersion, maxLoadWeight, currentLoad = null) {
+  constructor(
+    name, 
+    weight, 
+    coords,
+    chipVersion, 
+    maxLoadWeight, 
+    currentLoad = null
+    ) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
