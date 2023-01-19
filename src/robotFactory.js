e37436b8fcd5f9e508f927979fc7f48a509c3605
Ihor Mariash <imariash86@gmail.com>
2023-01-19 09:55:25 +0200

    code formatting was fixed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -11,23 +11,27 @@ class BaseRobot {
     };
     this.chipVersion = chipVersion;
   }
+
   goForward(step = 1) {
     this.coords.y += step;
   }
+
   goBack(step = 1) {
     this.coords.y -= step;
   }
+
   goLeft(step = 1) {
     this.coords.x -= step;
   }
+
   goRight(step = 1) {
     this.coords.x += step;
   }
+
   getInfo() {
-    return `Robot: ${this
-      .name}, Chip version: ${this
-      .chipVersion}, Weight: ${this
-      .weight}`;
+    return 'Robot: ' + this.name
+    + ', Chip version: ' + this.chipVersion
+    + ', Weight: ' + this.weight;
   }
 }
 
@@ -44,26 +48,36 @@ class FlyingRobot extends BaseRobot {
     };
     this.chipVersion = chipVersion;
   }
+
   goUp(step = 1) {
     this.coords.z += step;
   }
+
   goDown(step = 1) {
     this.coords.z -= step;
   }
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion,
-    maxLoadWeight, currentLoad = null) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
   }
+
   hookLoad(cargo) {
     if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
   }
+
   unhookLoad() {
     this.currentLoad = null;
   }
