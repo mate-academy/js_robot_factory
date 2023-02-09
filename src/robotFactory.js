e35964bdb3eab840a8a7784500e3421dec327f8c
mykolachupyak <chupyak@gmail.com>
2023-02-09 19:24:29 +0200

    BaseRobot - all tests are passed

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,6 +1,34 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords = {}, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
+    this.coords = coords;
+    this.coords.x = coords.x === undefined ? 0 : coords.x;
+    this.coords.y = coords.y === undefined ? 0 : coords.y;
+  };
+  goForward(step = 1) {
+    this.coords.y += step;
+  };
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  };
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  };
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  };
+  getInfo() {
+    // eslint-disable-next-line max-len
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  };
+}
 
 class FlyingRobot {}
 
