908162e499f34241745103d44911e589380860c3
Roshan Jha <kartohajha@gmail.com>
2023-02-12 12:36:34 +0200

    refactoring done

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -5,10 +5,18 @@ class BaseRobot {
     this.name = name;
     this.weight = weight;
 
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
+    if (coords === undefined) {
+      this.coords = {
+        x: 0,
+        y: 0,
+      };
+    } else {
+      this.coords = {
+        x: (coords.x === undefined) ? 0 : coords.x,
+        y: (coords.y === undefined) ? 0 : coords.y,
+      };
     };
+
     this.chipVersion = chipVersion;
   }
 
@@ -38,6 +46,20 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
     this.coords.z = coords.z || 0;
+
+    if (coords === undefined) {
+      this.coords = {
+        x: 0,
+        y: 0,
+        z: 0,
+      };
+    } else {
+      this.coords = {
+        x: (coords.x === undefined) ? 0 : coords.x,
+        y: (coords.y === undefined) ? 0 : coords.y,
+        z: (coords.z === undefined) ? 0 : coords.z,
+      };
+    }
   }
 
   goUp(step = 1) {
