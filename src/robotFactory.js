4d0df85447c9ef6551bf1291858ea0eecb1b6fe5
Roshan Jha <kartohajha@gmail.com>
2023-02-14 13:18:10 +0200

    done

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,20 +1,13 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
+  constructor(name, weight, { x = 0, y = 0 }, chipVersion) {
     this.name = name;
     this.weight = weight;
 
-    if (coords === undefined) {
-      this.coords = {
-        x: 0,
-        y: 0,
-      };
-    } else {
-      this.coords = {
-        x: (coords.x === undefined) ? 0 : coords.x,
-        y: (coords.y === undefined) ? 0 : coords.y,
-      };
+    this.coords = {
+      x: x,
+      y: y,
     };
 
     this.chipVersion = chipVersion;
@@ -43,23 +36,16 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
-    super(name, weight, coords, chipVersion);
-    this.coords.z = coords.z || 0;
+  constructor(name, weight, { x, y, z = 0 }, chipVersion) {
+    super(name, weight, {
+      x, y,
+    }, chipVersion);
 
-    if (coords === undefined) {
-      this.coords = {
-        x: 0,
-        y: 0,
-        z: 0,
-      };
-    } else {
-      this.coords = {
-        x: (coords.x === undefined) ? 0 : coords.x,
-        y: (coords.y === undefined) ? 0 : coords.y,
-        z: (coords.z === undefined) ? 0 : coords.z,
-      };
-    }
+    this.coords = {
+      x,
+      y,
+      z,
+    };
   }
 
   goUp(step = 1) {
