de2ba6d20f787e2b765bf9a971cd73db37686d61
Serhii Nikolin <sergeynikolin@gmail.com>
2023-02-27 14:36:26 +0200

    Task done

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,99 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.coords = coords;
+    this.coords.x = this.coords.x ? this.coords.x : 0;
+    this.coords.y = this.coords.y ? this.coords.y : 0;
+    this.chipVersion = chipVersion;
+  }
 
-class FlyingRobot {}
+  goRight(value = 1) {
+    if (value > 0) {
+      this.coords.x += value;
+    };
 
-class DeliveryDrone {}
+    return this;
+  };
+
+  goLeft(value = 1) {
+    if (value > 0) {
+      this.coords.x += -value;
+    };
+
+    return this;
+  };
+
+  goBack(value = 1) {
+    if (value > 0) {
+      this.coords.y += -value;
+    };
+
+    return this;
+  };
+
+  goForward(value = 1) {
+    if (value > 0) {
+      this.coords.y += value;
+    };
+
+    return this;
+  };
+
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, `
+    + `Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords.z = this.coords.z ? this.coords.z : 0;
+  }
+
+  goUp(value = 1) {
+    if (value > 0) {
+      this.coords.z += value;
+    };
+
+    return this;
+  };
+
+  goDown(value = 1) {
+    if (value > 0) {
+      this.coords.z += -value;
+    };
+
+    return this;
+  };
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight,
+    currentLoad = null) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (cargo.weight <= this.maxLoadWeight
+      && this.currentLoad === null) {
+      this.currentLoad = cargo;
+    }
+
+    return this.currentLoad;
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+
+    return this.currentLoad;
+  }
+}
 
 module.exports = {
   BaseRobot,
