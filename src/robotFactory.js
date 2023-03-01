c736c3f2a2cab148582fae0fb73edaedd4556251
gretoq <dovhun3d@gmail.com>
2023-03-01 13:46:00 +0200

    Created a robot factory, which makes robots which have some abilities

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,129 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(
+    name,
+    weight,
 
-class FlyingRobot {}
+    {
+      x = 0,
+      y = 0,
+    } = {},
 
-class DeliveryDrone {}
+    chipVersion,
+  ) {
+    this.name = name;
+    this.weight = weight;
+
+    this.coords = {
+      x,
+      y,
+    };
+
+    this.chipVersion = chipVersion;
+  }
+
+  getInfo() {
+    return `Robot: ${this.name}, `
+      + `Chip version: ${this.chipVersion}, `
+      + `Weight: ${this.weight}`;
+  }
+
+  goForward(step = 1) {
+    this.coords.y += step;
+  }
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  }
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  }
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(
+    name,
+    weight,
+    {
+      x = 0,
+      y = 0,
+      z = 0,
+    } = {},
+    chipVersion,
+  ) {
+    super(
+      name,
+      weight,
+      {
+        x,
+        y,
+      },
+      chipVersion,
+    );
+
+    this.coords = {
+      x,
+      y,
+      z,
+    };
+  }
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  }
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(
+    name,
+    weight,
+    {
+      x = 0,
+      y = 0,
+      z = 0,
+    } = {},
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null,
+  ) {
+    super(
+      name,
+      weight,
+      {
+        x,
+        y,
+        z,
+      },
+      chipVersion,
+    );
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    const weight = cargo.weight;
+    const islessMaxLoad = weight <= this.maxLoadWeight;
+
+    if (this.currentLoad === null && islessMaxLoad) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
