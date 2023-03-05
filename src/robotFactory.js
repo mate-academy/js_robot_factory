a39d00f5118e9efcc861407412a7bf22e4c72d0b
Andrii Kuznetsov <andreykuzmich91@gmail.com>
2023-03-05 12:47:44 +0200

    all robots

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,7 +1,9 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, { x = 0, y = 0 }, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
+    const { x = 0, y = 0 } = coords;
+
     this.name = name;
     this.weight = weight;
 
@@ -34,9 +36,49 @@ class BaseRobot {
   }
 }
 
-class FlyingRobot {}
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    const { x = 0, y = 0, z = 0 } = coords;
 
-class DeliveryDrone {}
+    super(
+      name,
+      weight,
+      {
+        x,
+        y,
+      },
+      chipVersion);
+    this.coords.z = z;
+  }
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  }
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(
+    name, weight, coords, chipVersion, maxLoadWeight, currentLoad = null) {
+    super(name, weight, coords, chipVersion);
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
