e246df6f1d72395945a902270f90a3a899faa440
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-03-06 12:19:07 +0200

    Solution fixed v1.0.2

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -36,12 +36,20 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
-    super(name, weight, coords, chipVersion);
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion
+  ) {
+    super(
+      name,
+      weight,
+      coords,
+      chipVersion
+    );
 
-    const { z = 0 } = coords;
-
-    this.coords.z = z;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -54,10 +62,22 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
-    super(name, weight, coords, chipVersion);
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null,
+  ) {
+    super(
+      name,
+      weight,
+      coords,
+      chipVersion
+    );
 
-    this.currentLoad = currentLoad || null;
+    this.currentLoad = currentLoad;
     this.maxLoadWeight = maxLoadWeight;
   }
 
