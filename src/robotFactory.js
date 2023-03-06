dd0f07e02a0f5bb25393998f4d96024d7bbede3c
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-03-06 21:51:58 +0200

    Soltion v4

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -2,7 +2,8 @@
 
 class BaseRobot {
   constructor(name, weight, coords = {
-    x: 0, y: 0,
+    x: 0,
+    y: 0,
   }, chipVersion) {
     this.name = name;
     this.weight = weight;
@@ -38,7 +39,9 @@ class BaseRobot {
 
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords = {
-    x: 0, y: 0, z: 0,
+    x: 0,
+    y: 0,
+    z: 0,
   }, chipVersion) {
     super(name, weight, coords, chipVersion);
     this.coords.z = coords.z || 0;
@@ -55,7 +58,9 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(name, weight, coords = {
-    x: 0, y: 0, z: 0,
+    x: 0,
+    y: 0,
+    z: 0,
   }, chipVersion, maxLoadWeight, currentLoad = null) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
