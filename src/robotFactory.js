f87d14a286ddead65a5a88f067fae196ec46716d
vadym4che <vadym4che@gmail.com>
2023-03-23 19:16:58 +0200

    refactored according suggested aproach

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,13 +1,19 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
+  constructor(
+    name,
+    weight,
+    { x = 0, y = 0 },
+    chipVersion
+  ) {
     this.name = name;
     this.weight = weight;
 
-    this.coords = Object.assign({
-      x: 0, y: 0,
-    }, coords);
+    this.coords = {
+      x,
+      y,
+    };
     this.chipVersion = chipVersion;
   }
   goForward(step = 1) {
@@ -31,12 +37,17 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion
+  ) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = Object.assign({
-      x: 0, y: 0, z: 0,
-    }, coords);
+    const { z = 0 } = coords;
+
+    this.coords.z = z;
   }
 
   goUp(step = 1) {
@@ -49,12 +60,16 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(
-    name, weight, coords, chipVersion,
-    maxLoadWeight, currentLoad = null
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null,
   ) {
     super(name, weight, coords, chipVersion);
-    this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
+    this.maxLoadWeight = maxLoadWeight;
   }
   hookLoad(cargo) {
     if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
