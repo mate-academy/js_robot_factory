fcf2bab33c4a11e4dd03fdf0f204a981dc94a3d0
Arthur Krepko <krepko2003krepko@gmail.com>
2023-03-26 23:42:25 +0300

    reject considered

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,13 +1,13 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
+  constructor(name, weight, { x = 0, y = 0 }, chipVersion) {
     this.name = name;
     this.weight = weight;
 
     this.coords = {
-      'x': coords.x || 0,
-      'y': coords.y || 0,
+      x,
+      y,
     };
 
     this.chipVersion = chipVersion;
@@ -36,13 +36,18 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
-    super(name, weight, coords, chipVersion);
+  constructor(name, weight, { x = 0, y = 0, z = 0 }, chipVersion) {
+    super(
+      name,
+      weight,
+      {
+        x,
+        y,
+      },
+      chipVersion
+    );
 
-    this.coords = {
-      ...coords,
-      'z': coords.z || 0,
-    };
+    this.coords.z = z;
   }
 
   goUp(step = 1) {
