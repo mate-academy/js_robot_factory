3a4e2d21d45b40444b27288db3b6dca51121c582
Daryna Hnidash <melissa998877@gmail.com>
2023-03-29 14:40:07 +0300

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,88 @@
+/* eslint-disable max-len */
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
 
-class FlyingRobot {}
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
+    this.chipVersion = chipVersion;
+  }
 
-class DeliveryDrone {}
+  goForward(value = 1) {
+    if (value > 0) {
+      this.coords.y += value;
+    }
+  };
+
+  goBack(value = 1) {
+    if (value > 0) {
+      this.coords.y -= value;
+    }
+  };
+
+  goRight(value = 1) {
+    if (value > 0) {
+      this.coords.x += value;
+    }
+  };
+
+  goLeft(value = 1) {
+    if (value > 0) {
+      this.coords.x = this.coords.x - value;
+    }
+  };
+
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    this.coords.z = coords.z || 0;
+  };
+
+  goUp(value = 1) {
+    this.coords.z += value;
+  };
+
+  goDown(value = 1) {
+    this.coords.z -= value;
+  };
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
+    super(name, weight, coords, chipVersion);
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    };
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
