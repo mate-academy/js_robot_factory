e67a03b9d4fbad9577e60ffde85c682e92d6af5e
Andrii <remote.makandriival@gmail.com>
2023-04-04 16:25:51 +0300

    fixes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -9,6 +9,7 @@ class BaseRobot {
       x: coords.x || 0,
       y: coords.y || 0,
     };
+
     this.chipVersion = chipVersion;
   }
 
@@ -38,7 +39,12 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords, chipVersion) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion
+  ) {
     super(name, weight, coords, chipVersion);
     this.coords.z = coords.z || 0;
   }
@@ -53,16 +59,21 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords = {
-    x: 0, y: 0,
-  }, chipVersion, maxLoadWeight, currentLoad = null) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
   }
 
   hookLoad(cargo) {
-    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
   }
