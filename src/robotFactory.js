459bbc101cb9bdc130e65eee19a511f51c135ba1
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-05 21:34:52 +0300

    Fix coords in Class

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -4,42 +4,38 @@ class BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     this.name = name;
     this.weight = weight;
-    this.coords = this.chekCoords(coords);
+
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
     this.chipVersion = chipVersion;
   }
-  
-  chekCoords(coords) {
-  if (Object.keys(coords).length === 0) {
-    return {x : 0, y : 0};
-    } else if (!Object.keys(coords).includes('x')) {
-      return { x : 0 , y: coords.y};
-    } else if (!Object.keys(coords).includes('y')) {
-      return { x : coords.x, y: 0};
-    }
 
-    return coords
-}
-  
   goForward(step = 1) {
     this.coords.y += step;
+
     return this;
   }
 
   goBack(step = 1) {
     this.coords.y -= step;
+
     return this;
   }
-    
+
   goLeft(step = 1) {
     this.coords.x -= step;
+
     return this;
   }
-  
+
   goRight(step = 1) {
     this.coords.x += step;
+
     return this;
   }
-  
+
   getInfo() {
     const textName = `Robot: ${this.name}, `;
     const textChipVersion = `Chip version: ${this.chipVersion}, `;
@@ -52,37 +48,36 @@ class BaseRobot {
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
-    
-    this.coords = this.chekAllCoords(coords);
+
+    this.coords = this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+      z: coords.z || 0,
+    }; ;
   }
-  
-  
-  chekAllCoords(coords) {
-    const resultObj = {};
-    const keysCoords = Object.keys(coords);
-    const coordsArr = ['x','y','z'];
-    for (let i = 0; i < coordsArr.length; i++) {
-      if(!keysCoords.includes(coordsArr[i])) {
-        resultObj[coordsArr[i]] = 0;
-      }
-    }
-     return {...coords, ...resultObj};
-}
 
   goUp(step = 1) {
     this.coords.z += step;
+
     return this;
   }
 
   goDown(step = 1) {
     this.coords.z -= step;
+
     return this;
   }
-
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad = null) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
@@ -90,8 +85,8 @@ class DeliveryDrone extends FlyingRobot {
   }
 
   hookLoad(cargo) {
-    if(cargo.weight <= this.maxLoadWeight && !this.currentLoad) {
-      return this.currentLoad = cargo;
+    if (cargo.weight <= this.maxLoadWeight && !this.currentLoad) {
+      this.currentLoad = cargo;
     }
   }
 
