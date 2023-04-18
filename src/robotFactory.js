e794ff02968cf77092de99caff331f4c9d8fb250
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-04-18 16:00:20 +0200

    solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -40,9 +40,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords.z = coords.z
-      ? coords.z
-      : 0;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -64,8 +62,7 @@ class DeliveryDrone extends FlyingRobot {
   }
 
   hookLoad(cargo) {
-    if (this.currentLoad === null
-      && cargo.weight <= this.maxLoadWeight) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
   }
