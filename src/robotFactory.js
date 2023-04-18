1b8001f3e4bb524a628924f62b6c2e8adb4ef379
Paweł Smoliński <ski1993@o2.pl>
2023-04-18 12:55:04 +0200

    requested changes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -6,7 +6,7 @@ class BaseRobot {
     this.weight = weight;
 
     this.coords = {
-      x: x, y: y,
+      x, y,
     };
     this.chipVersion = chipVersion;
   }
@@ -36,11 +36,11 @@ class BaseRobot {
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, { x = 0, y = 0, z = 0 }, chipVersion) {
     super(name, weight, {
-      x: x, y: y,
+      x, y,
     }, chipVersion);
 
     this.coords = {
-      x: x, y: y, z: z,
+      x, y, z,
     };
   }
 
@@ -54,17 +54,15 @@ class FlyingRobot extends BaseRobot {
 };
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, { x = 0, y = 0, z = 0 },
+  constructor(name, weight, coords,
     chipVersion, maxLoadWeight, currentLoad = null) {
-    super(name, weight, {
-      x: x, y: y, z: z,
-    }, chipVersion);
+    super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
   };
 
   hookLoad(cargo) {
-    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
   };
