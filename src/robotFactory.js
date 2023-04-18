daf09e1157a755cdf21a136193922248dfd17a6d
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-04-18 20:51:14 +0200

    add solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,95 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords = {}, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
 
-class FlyingRobot {}
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
+  }
 
-class DeliveryDrone {}
+  goForward(stepForward = 1) {
+    if (stepForward > 0) {
+      this.coords.y += stepForward;
+    }
+
+    return this;
+  };
+
+  goBack(stepBack = 1) {
+    if (stepBack > 0) {
+      this.coords.y -= stepBack;
+    }
+
+    return this;
+  };
+
+  goRight(stepRight = 1) {
+    if (stepRight > 0) {
+      this.coords.x += stepRight;
+    }
+
+    return this;
+  };
+
+  goLeft(stepLeft = 1) {
+    if (stepLeft > 0) {
+      this.coords.x -= stepLeft;
+    }
+
+    return this;
+  };
+
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: `
+    + `${this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    this.coords.z = coords.z || 0;
+  }
+
+  goUp(stepUp = 1) {
+    this.coords.z += stepUp;
+  }
+  goDown(stepDown = 1) {
+    this.coords.z -= stepDown;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
+    super(name, weight, coords, chipVersion);
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+
+  hookLoad(cargo) {
+    if (!this.currentLoad && this.maxLoadWeight >= cargo.weight) {
+      this.currentLoad = cargo;
+    }
+  }
+}
 
 module.exports = {
   BaseRobot,
