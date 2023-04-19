2d347a6f25f736fdcfaf98ffbd41fd1e94ac11d1
Natalia Klonowska <natalia742@gmail.com>
2023-04-19 13:54:26 +0200

    update

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -24,21 +24,15 @@ class BaseRobot {
     this.coords.x -= step;
   }
   getInfo() {
-    return 'Robot: ' + this.name
-    + ', Chip version: ' + this.chipVersion
-    + ', Weight: ' + this.weight;
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, `
+    + `Weight: ${this.weight}`;
   }
 }
 
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
-
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
   goUp(step = 1) {
     this.coords.z += step;
