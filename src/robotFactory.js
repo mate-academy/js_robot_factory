9d7bb215fc53c81f9ab529bb9f48b8ef0eaf0a04
Kacper Łyczba <suaby1725@gmail.com>
2023-04-21 02:33:35 +0200

    fixes

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -7,13 +7,10 @@ class BaseRobot {
     this.coords = coords;
     this.chipVersion = chipVersion;
 
-    if (!this.coords.x) {
-      this.coords.x = 0;
-    }
-
-    if (!this.coords.y) {
-      this.coords.y = 0;
-    }
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
   }
 
   goForward(step = 1) {
@@ -39,12 +36,9 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords = {}, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
-
-    if (!this.coords.z) {
-      this.coords.z = 0;
-    }
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -57,7 +51,7 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords = {}, chipVersion,
+  constructor(name, weight, coords, chipVersion,
     maxLoadWeight, currentLoad = null) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
