648c9d99e9d5e4a91137eee05002ff6192f0c3be
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-04-22 07:40:03 +0200

    make corrections as per the review comments

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -35,11 +35,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
   goUp(step = 1) {
     this.coords.z += step;
@@ -62,7 +58,7 @@ class DeliveryDrone extends FlyingRobot {
       return;
     }
 
-    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
   }
