9008912b28f3214de43fa842c1c283d2b14cb6d5
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-04-23 22:51:48 +0300

    used desctucturing in getInfo, and removed some unnecessary code

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -14,51 +14,46 @@ class BaseRobot {
 
   goForward(step = 1) {
     this.coords.y += step;
-  };
+  }
 
   goBack(step = 1) {
     this.coords.y -= step;
-  };
+  }
 
   goRight(step = 1) {
     this.coords.x += step;
-  };
+  }
 
   goLeft(step = 1) {
     this.coords.x -= step;
-  };
+  }
 
   getInfo() {
-    return `Robot: ${this.name}, Chip version: ${
-      this.chipVersion}, Weight: ${this.weight}`;
+    const { name, chipVersion, weight } = this;
+
+    return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
   }
 }
 
 class FlyingRobot extends BaseRobot {
-
-  constructor(
-    name,
-    weight,
-    coords,
-    chipVersion
-  ) {
+  constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
     this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
     this.coords.z += step;
-  };
+  }
   goDown(step = 1) {
     this.coords.z -= step;
-  };
+  }
 }
 
 class DeliveryDrone extends FlyingRobot {
   constructor(
     name,
     weight,
-    coords = 0,
+    coords,
     chipVersion,
     maxLoadWeight,
     currentLoad = null
@@ -68,8 +63,7 @@ class DeliveryDrone extends FlyingRobot {
     this.currentLoad = currentLoad;
   }
   hookLoad(cargo) {
-    if ((cargo.weight <= this.maxLoadWeight)
-      && this.currentLoad === null) {
+    if (cargo.weight <= this.maxLoadWeight && !this.currentLoad) {
       this.currentLoad = cargo;
     }
   }
