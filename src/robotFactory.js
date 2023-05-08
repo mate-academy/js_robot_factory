a474339fda4bb82cc9880d6e2c1f11962dbaba4e
Nazar Yants <nazyants@gmail.com>
2023-05-08 12:05:05 +0300

    fix after review

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -47,12 +47,7 @@ class FlyingRobot extends BaseRobot {
     chipVersion
   ) {
     super(name, weight, coords, chipVersion);
-
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
