b92d5b3e88b92e3da5f493727ef2b1a5455ec19c
AndriiYelieva <andrii.yelieva@gmail.com>
2023-05-15 21:58:24 +0300

    exactly Done.

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -14,8 +14,8 @@ class BaseRobot {
 
   getInfo() {
     return (
-      `Robot: ${this.name},`
-      + ` Chip version: ${this.chipVersion}, Weight: ${this.weight}`
+      `Robot: ${this.name}, Chip version: ${
+        this.chipVersion}, Weight: ${this.weight}`
     );
   }
 
@@ -37,11 +37,7 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords = {
-      x: coords.x || 0,
-      y: coords.y || 0,
-      z: coords.z || 0,
-    };
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
