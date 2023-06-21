269b37799eae533c0bae76065071f35fbc3294ff
spencerspe <spencerspe@gmail.com>
2023-06-21 12:45:11 +0100

    second version

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,42 +1,40 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, coords = {
-    x: 0,
-    y: 0,
-  }, chipVersion) {
+  constructor(
+    name,
+    weight,
+    coords = {
+      x: 0,
+      y: 0,
+    }, chipVersion) {
     this.name = name;
     this.weight = weight;
 
     this.coords = {
-      x: 0,
-      y: 0,
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
 
-    for (const value in coords) {
-      this.coords[value] = coords[value];
-    }
-
-    for (const coord in this.coords) {
-      if (coord === undefined) {
-        this.coords[coord] = 0;
-      }
-    }
     this.chipVersion = chipVersion;
   }
 
   goForward(step = 1) {
     this.coords.y += step;
   }
+
   goBack(step = 1) {
     this.coords.y -= step;
   }
+
   goRight(step = 1) {
     this.coords.x += step;
   }
+
   goLeft(step = 1) {
     this.coords.x -= step;
   }
+
   getInfo() {
     return `Robot: ${this.name}, Chip version: ${this.chipVersion}, `
     + `Weight: ${this.weight}`;
@@ -44,37 +42,48 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, coords = {
-    x: 0,
-    y: 0,
-    z: 0,
-  }, chipVersion) {
+  constructor(
+    name,
+    weight,
+    coords = {
+      x: 0,
+      y: 0,
+    }, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    if (this.coords.z === undefined) {
-      this.coords.z = 0;
-    }
+    this.coords.z = coords.z || 0;
   }
+
   goUp(step = 1) {
     this.coords.z += step;
   }
+
   goDown(step = 1) {
     this.coords.z -= step;
   }
 }
-
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords = {
-    x: 0, y: 0, z: 0,
-  }, chipVersion, maxLoadWeight, currentLoad = null) {
-    super(name, weight, coords, chipVersion);
+  constructor(
+    name,
+    weight,
+    coords = {
+      x: 0,
+      y: 0,
+      z: 0,
+    }, chipVersion,
+    maxLoadWeight,
+    currentLoad = null) {
+    super(
+      name,
+      weight,
+      coords,
+      chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
-    this.coords.z = coords.z;
   }
 
   hookLoad(cargo) {
-    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+    if (!this.currentLoad && cargo.weight <= this.maxLoadWeight) {
       this.currentLoad = cargo;
     }
   }
