064981b92c87be0d240e54498d66fb5c932db66c
Eugene Kulish <genekulish13@gmail.com>
2023-06-22 15:19:27 +0300

    solve the task

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -2,13 +2,20 @@
 
 class BaseRobot {
   constructor(
-    name, weight, { x = 0, y = 0 }, chipVersion
+    name,
+    weight,
+    {
+      x = 0,
+      y = 0,
+    },
+    chipVersion
   ) {
     this.name = name;
     this.weight = weight;
 
     this.coords = {
-      x, y,
+      x,
+      y,
     };
     this.chipVersion = chipVersion;
   }
@@ -37,11 +44,24 @@ Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
 
 class FlyingRobot extends BaseRobot {
   constructor(
-    name, weight, { x = 0, y = 0, z = 0 }, chipVersion
+    name,
+    weight,
+    {
+      x = 0,
+      y = 0,
+      z = 0,
+    },
+    chipVersion
   ) {
-    super(name, weight, {
-      x, y,
-    }, chipVersion);
+    super(
+      name,
+      weight,
+      {
+        x,
+        y,
+      },
+      chipVersion
+    );
     this.coords.z = z;
   }
 
@@ -56,19 +76,36 @@ class FlyingRobot extends BaseRobot {
 
 class DeliveryDrone extends FlyingRobot {
   constructor(
-    name, weight, { x = 0, y = 0, z = 0 },
-    chipVersion, maxLoadWeight, currentLoad = null
+    name,
+    weight,
+    {
+      x = 0,
+      y = 0,
+      z = 0,
+    },
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
   ) {
-    super(name, weight, {
-      x, y, z,
-    }, chipVersion);
+    super(
+      name,
+      weight,
+      {
+        x,
+        y,
+        z,
+      },
+      chipVersion
+    );
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
   }
 
   hookLoad(cargo) {
     this.currentLoad
-      = (cargo.weight <= this.maxLoadWeight && !this.currentLoad)
+      = (
+        cargo.weight <= this.maxLoadWeight && !this.currentLoad
+      )
         ? cargo
         : this.currentLoad;
   }
