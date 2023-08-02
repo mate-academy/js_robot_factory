eac7267736ac6315e78273170c372029d73e7a8c
Maksym Bevskyi <bevskiymaks@gmail.com>
2023-08-02 13:53:49 +0200

    Add tusk solution.

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,10 +1,96 @@
 'use strict';
 
-class BaseRobot {}
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
 
-class FlyingRobot {}
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
+    this.chipVersion = chipVersion;
+  }
 
-class DeliveryDrone {}
+  getInfo() {
+    const { name, chipVersion, weight } = this;
+
+    return `Robot: ${name}, Chip version: ${chipVersion}, Weight: ${weight}`;
+  }
+
+  goForward(step) {
+    if (step) {
+      this.coords.y += step;
+    } else {
+      this.coords.y++;
+    }
+  }
+
+  goBack(step) {
+    if (step) {
+      this.coords.y -= step;
+    } else {
+      this.coords.y--;
+    }
+  }
+
+  goRight(step) {
+    if (step) {
+      this.coords.x += step;
+    } else {
+      this.coords.x++;
+    }
+  }
+
+  goLeft(step) {
+    if (step) {
+      this.coords.x -= step;
+    } else {
+      this.coords.x--;
+    }
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords.z = coords.z || 0;
+  }
+
+  goUp(step) {
+    if (step) {
+      this.coords.z += step;
+    } else {
+      this.coords.z++;
+    }
+  }
+
+  goDown(step) {
+    if (step) {
+      this.coords.z -= step;
+    } else {
+      this.coords.z--;
+    }
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad) {
+    super(name, weight, coords, chipVersion);
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad || null;
+  }
+
+  hookLoad(cargo) {
+    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    }
+  }
+
+  unhookLoad() {
+    this.currentLoad = null;
+  }
+}
 
 module.exports = {
   BaseRobot,
