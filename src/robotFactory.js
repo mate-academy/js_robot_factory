9029ecb92650e1e7c26f5dbe6696e8d3f9b111b5
Anton Tsurkan <wesses1999@gmail.com>
2023-08-16 11:22:39 +0300

    fix

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -36,8 +36,8 @@ class BaseRobot {
 
 class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
-    coords.z = coords.z || 0;
     super(name, weight, coords, chipVersion);
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -50,12 +50,13 @@ class FlyingRobot extends BaseRobot {
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords = {
-    x: 0,
-    y: 0,
-    z: 0,
-  },
-  chipVersion, maxLoadWeight, currentLoad = null) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null) {
     super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
