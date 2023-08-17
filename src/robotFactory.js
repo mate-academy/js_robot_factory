f2fb7e6be5928072be8b45984818a2ad12e8eece
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-08-17 21:58:04 +0300

    replace unnesecary arguments

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,14 +1,14 @@
 'use strict';
 
 class BaseRobot {
-  constructor(name, weight, { x = 0, y = 0 }, chipVersion) {
+  constructor(name, weight, coords, chipVersion) {
     this.name = name;
     this.weight = weight;
     this.chipVersion = chipVersion;
 
     this.coords = {
-      x,
-      y,
+      x: coords.x || 0,
+      y: coords.y || 0,
     };
   }
 
@@ -36,12 +36,10 @@ class BaseRobot {
 }
 
 class FlyingRobot extends BaseRobot {
-  constructor(name, weight, { x = 0, y = 0, z = 0 }, chipVersion) {
-    super(name, weight, {
-      x, y,
-    }, chipVersion);
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
 
-    this.coords.z = z;
+    this.coords.z = coords.z || 0;
   }
 
   goUp(step = 1) {
@@ -57,14 +55,12 @@ class DeliveryDrone extends FlyingRobot {
   constructor(
     name,
     weight,
-    { x = 0, y = 0, z = 0 },
+    coords,
     chipVersion,
     maxLoadWeight,
     currentLoad = null
   ) {
-    super(name, weight, {
-      x, y, z,
-    }, chipVersion);
+    super(name, weight, coords, chipVersion);
     this.maxLoadWeight = maxLoadWeight;
     this.currentLoad = currentLoad;
   }
