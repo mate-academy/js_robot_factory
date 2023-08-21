1a86514365aa176d38e6370afda322034cfb267f
vanom <envinenvin@gmail.com>
2023-08-21 13:16:19 +0300

    add task solution

--- src/robotFactory.js
+++ src/robotFactory.js
@@ -1,17 +1,20 @@
 'use strict';
 
-const ROBOT_BASE_COORD = 0;
+const ROBOT_BASE_COORDS = {
+  x: 0,
+  y: 0,
+  z: 0,
+};
 const BASE_STEP = 1;
 
 class BaseRobot {
   constructor(name, weight, coords, chipVersion) {
+    this.coords = {
+      x: coords.x || ROBOT_BASE_COORDS.x,
+      y: coords.y || ROBOT_BASE_COORDS.y,
+    };
     this.name = name;
     this.weight = weight;
-
-    this.coords = {
-      x: coords.x || ROBOT_BASE_COORD,
-      y: coords.y || ROBOT_BASE_COORD,
-    };
     this.chipVersion = chipVersion;
   }
 
@@ -23,26 +26,18 @@ class BaseRobot {
 
   goForward(step = BASE_STEP) {
     this.coords.y += step;
-
-    return this;
   }
 
   goBack(step = BASE_STEP) {
     this.coords.y -= step;
-
-    return this;
   }
 
   goRight(step = BASE_STEP) {
     this.coords.x += step;
-
-    return this;
   }
 
   goLeft(step = BASE_STEP) {
     this.coords.x -= step;
-
-    return this;
   }
 }
 
@@ -50,25 +45,27 @@ class FlyingRobot extends BaseRobot {
   constructor(name, weight, coords, chipVersion) {
     super(name, weight, coords, chipVersion);
 
-    this.coords.z = coords.z || ROBOT_BASE_COORD;
+    this.coords.z = coords.z || ROBOT_BASE_COORDS.z;
   }
 
   goUp(step = BASE_STEP) {
     this.coords.z += step;
-
-    return this;
   }
 
   goDown(step = BASE_STEP) {
     this.coords.z -= step;
-
-    return this;
   }
 }
 
 class DeliveryDrone extends FlyingRobot {
-  constructor(name, weight, coords, chipVersion,
-    maxLoadWeight, currentLoad = null) {
+  constructor(
+    name,
+    weight,
+    coords,
+    chipVersion,
+    maxLoadWeight,
+    currentLoad = null
+  ) {
     super(name, weight, coords, chipVersion);
 
     this.maxLoadWeight = maxLoadWeight;
